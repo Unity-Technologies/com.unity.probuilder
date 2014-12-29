655f66915954b8df01098294ed7bddc1b5970beb
Karl Henkel <karl-@users.noreply.github.com>
2014-12-29 16:15:48 +0000

    2.0: Add pbcore unity 4 vs proj. re #563

---
+++ visual studio/ProBuilderCore-Unity4/ProBuilderCore-Unity4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProBuilderCore-Unity4")]
+[assembly: AssemblyDescription("ProBuilder Core Lib")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ProCore")]
+[assembly: AssemblyProduct("ProBuilderCore")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5ce6a97-8c9d-474b-809e-f097a4f2f2c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
