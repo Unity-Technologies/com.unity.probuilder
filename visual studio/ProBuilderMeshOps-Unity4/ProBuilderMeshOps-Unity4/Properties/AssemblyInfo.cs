c5483699a186a3e5b6aa3f2ce330f3ce39dcb715
Karl Henkel <karl-@users.noreply.github.com>
2014-12-31 14:22:52 +0000

    2.0: unity 4/5 projects for mesh ops and editor libs. re #536 Time:2h30m

--- visual studio/ProBuilderMeshOps-Unity4/ProBuilderMeshOps-Unity4/Properties/AssemblyInfo.cs
+++ visual studio/ProBuilderMeshOps-Unity4/ProBuilderMeshOps-Unity4/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProBuilderEditor")]
+[assembly: AssemblyTitle("ProBuilderMeshOps-Unity4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProBuilderEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("ProBuilderMeshOps-Unity4")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e3ab02c-54f5-4d04-ac55-115f6be30a41")]
+[assembly: Guid("5e3cb53d-4865-482b-b875-97a155b26d07")]
 
 // Version information for an assembly consists of the following four values:
 //
