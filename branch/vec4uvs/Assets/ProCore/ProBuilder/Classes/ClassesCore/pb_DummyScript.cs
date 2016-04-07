8781e7185c16410d8b8f16a0e16d8125d6afb0e1
Karl Henkel <karl-@users.noreply.github.com>
2016-04-07 18:00:20 +0000

    pb: delete vec4 uv branch.

--- branch/vec4uvs/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_DummyScript.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-/**
- * An empty script used to provide some reference for null components so that we can delete them.
- */
-[AddComponentMenu("")]
-public class pb_DummyScript : MonoBehaviour {}
