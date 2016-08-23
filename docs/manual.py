d6f2847888019452568c67ae1033c9bf566155f0
karl <karlh07@gmail.com>
2016-08-23 11:18:55 -0400

    work on py script to compile pdfs from html docs.  currently builds all pages to separate pdfs

--- docs/manual.py
+++ docs/manual.py
@@ -3,12 +3,14 @@
 # pip install pdfkit
 # pip install pyyaml
 
+import os
 import pdfkit
 import yaml
 
 directory = yaml.load( open("mkdocs.yml") )
 pages = directory["pages"]
-
+pdf_dir = "pdfs"
+site_dir = "site"
 pdfs = []
 
 # gather all the docs pages in a list of tuples (category, title, path)
@@ -25,6 +27,55 @@ for header in pages:
 					for kvp in item:
 						pdfs.append( (key, kvp, item[kvp]) )
 
-print( str(pdfs) )
+if not os.path.exists(pdf_dir):
+    os.makedirs(pdf_dir)
 
-# pdfkit.from_file("site/index.html", "index.pdf")
+gen_pdfs = []
+
+for page in pdfs:
+	if page[1] == "Home" and page[2] == "index.md":
+		path = page[2].replace(".md", ".html")
+	else:
+		split = page[2].split("/")
+		path = page[2].replace(".md", "") + "/index.html"
+
+	if len(page[0]) > 0:
+		title = page[0] + " - " + page[1] + ".pdf"
+	else:
+		title = page[1] + ".pdf";
+
+	# print( path + " => " + pdf_dir + "/" + title)
+
+	# ignore exceptions because pdfkit (or more accurately, wkhtmltopdf) exits 1 with non-fatal
+	# errors
+	try:
+		gen_pdfs.append( pdfkit.from_file(site_dir + "/" + path, pdf_dir + "/" + title, options={ 'load-error-handling': 'ignore', 'disable-plugins':'' }) )
+	except:
+		pass
+
+print( str(gen_pdfs) )
+
+# [
+# 	('', 'Home', 'index.md'),
+# 	('', 'Fundamentals', 'general/fundamentals.md'),
+# 	('Toolbar', 'Overview', 'toolbar/overview-toolbar.md'),
+# 	('Toolbar', 'Tool Panels', 'toolbar/tool-panels.md'),
+# 	('Toolbar', 'Selection Actions', 'toolbar/selection-tools.md'),
+# 	('Toolbar', 'Object Actions', 'toolbar/object-actions.md'),
+# 	('Toolbar', 'Vertex Actions', 'toolbar/vertex.md'),
+# 	('Toolbar', 'Edge Actions', 'toolbar/edge.md'),
+# 	('Toolbar', 'Face Actions', 'toolbar/face.md'),
+# 	('Toolbar', 'Element Actions', 'toolbar/all.md'),
+# 	('Texture Mapping', 'Overview', 'texturing/overview-texture-mapping.md'),
+# 	('Texture Mapping', 'UV Editor Toolbar', 'texturing/uv-editor-toolbar.md'),
+# 	('Texture Mapping', 'Auto UVs Actions', 'texturing/auto-uvs-actions.md'),
+# 	('Texture Mapping', 'Manual UVs Actions', 'texturing/manual-uvs-actions.md'),
+# 	('', 'Preferences', 'preferences/preferences.md'),
+# 	('', 'Troubleshooting', 'troubleshooting/faq.md'),
+# 	('Upgrading', 'What Upgrade Procedure Should I Follow?', 'upgrading/overview.md'),
+# 	('Upgrading', 'Standard', 'upgrading/standard.md'),
+# 	('Upgrading', 'DLL Rename', 'upgrading/dllrename.md'),
+# 	('Upgrading', 'Upgrade Kit', 'upgrading/upgrade-kit.md'),
+# 	('Upgrading', 'Prototype', 'upgrading/prototype.md'),
+# 	('', 'Changelog', 'changelog.md')
+# ]
