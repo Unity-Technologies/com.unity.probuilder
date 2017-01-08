c3653187891e56d474b5b1b43f18fb31066e1ff6
= <karlh07@gmail.com>
2017-01-08 14:46:52 -0500

    Add meta files for extrusion modes.

--- probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_Enum.cs
+++ probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_Enum.cs
@@ -205,8 +205,8 @@ namespace ProBuilder2.Common
 	 */
 	public enum ExtrudeMethod
 	{
-		IndividualFaces,
-		VertexNormal,
-		FaceNormal
+		IndividualFaces	= 0,
+		VertexNormal	= 1,
+		FaceNormal		= 2
 	}
 }
