b92e96ed3378f3279d97347ad8957d4028086ff2
karl- <karlh07@gmail.com>
2017-03-24 10:53:24 -0400

    Hook up icons for Bezier and Poly shape buttons

--- probuilder2.0/Assets/ProCore/ProBuilder/Editor/EditorCore/MenuActions/Editors/NewPolyShape.cs
+++ probuilder2.0/Assets/ProCore/ProBuilder/Editor/EditorCore/MenuActions/Editors/NewPolyShape.cs
@@ -9,7 +9,7 @@ namespace ProBuilder2.Actions
 	public class NewPolyShape : pb_MenuAction
 	{
 		public override pb_ToolbarGroup group { get { return pb_ToolbarGroup.Tool; } }
-		public override Texture2D icon { get { return pb_IconUtility.GetIcon("Toolbar/Panel_Shapes"); } }
+		public override Texture2D icon { get { return pb_IconUtility.GetIcon("Toolbar/NewPolyShape"); } }
 		public override pb_TooltipContent tooltip { get { return _tooltip; } }
 		public override string menuTitle { get { return "New Poly Shape"; } }
 		public override int toolbarPriority { get { return 1; } }
