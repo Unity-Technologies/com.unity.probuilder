ee5db64ebd8a2180a9e0ed76027f9454a036ea95
karl- <karlh07@gmail.com>
2017-04-25 10:03:46 -0400

    Put pb_Lightmapping class in EditorCommon namespace

--- probuilder2.0/Assets/ProCore/ProBuilder/Editor/EditorCore/pb_Lightmapping.cs
+++ probuilder2.0/Assets/ProCore/ProBuilder/Editor/EditorCore/pb_Lightmapping.cs
@@ -3,69 +3,72 @@ using UnityEngine;
 using ProBuilder2.Common;
 using ProBuilder2.EditorCommon;
 
-/**
- * Methods used in manipulating or creating Lightmaps.
- */
-public static class pb_Lightmapping
+namespace ProBuilder2.EditorCommon
 {
 	/**
-	 * Editor-only extension to pb_Object generates lightmap UVs.
+	 * Methods used in manipulating or creating Lightmaps.
 	 */
-	[System.Obsolete("GenerateUV2 is obsolete, use pb_Editor_Mesh_Utility.Optimize(this pb_Object, bool forceRebuildUV2 = false) instead.")]
-	public static void GenerateUV2(this pb_Object pb) { pb.GenerateUV2(false); }
-
-	[System.Obsolete("GenerateUV2 is obsolete, use pb_Editor_Mesh_Utility.Optimize(this pb_Object, bool forceRebuildUV2 = false) instead.")]
-	public static void GenerateUV2(this pb_Object pb, bool forceUpdate)
+	public static class pb_Lightmapping
 	{
-		pb.Optimize(forceUpdate);
-	}
+		/**
+		 * Editor-only extension to pb_Object generates lightmap UVs.
+		 */
+		[System.Obsolete("GenerateUV2 is obsolete, use pb_Editor_Mesh_Utility.Optimize(this pb_Object, bool forceRebuildUV2 = false) instead.")]
+		public static void GenerateUV2(this pb_Object pb) { pb.GenerateUV2(false); }
 
-	/**
-	 *	Get the UnwrapParam values from a pb_UnwrapParameters object.
-	 *	Not in pb_UnwrapParameters because UnwrapParam is an Editor class.
-	 */
-	public static UnwrapParam GetUnwrapParam(pb_UnwrapParameters parameters)
-	{
-		UnwrapParam param;
-		UnwrapParam.SetDefaults(out param);
-
-		if(parameters != null)
+		[System.Obsolete("GenerateUV2 is obsolete, use pb_Editor_Mesh_Utility.Optimize(this pb_Object, bool forceRebuildUV2 = false) instead.")]
+		public static void GenerateUV2(this pb_Object pb, bool forceUpdate)
 		{
-			param.angleError = Mathf.Clamp(parameters.angleError, 1f, 75f) * .01f;
-			param.areaError  = Mathf.Clamp(parameters.areaError , 1f, 75f) * .01f;
-			param.hardAngle  = Mathf.Clamp(parameters.hardAngle , 0f, 180f);
-			param.packMargin = Mathf.Clamp(parameters.packMargin, 1f, 64) * .001f;
+			pb.Optimize(forceUpdate);
 		}
 
-		return param;
-	}
+		/**
+		 *	Get the UnwrapParam values from a pb_UnwrapParameters object.
+		 *	Not in pb_UnwrapParameters because UnwrapParam is an Editor class.
+		 */
+		public static UnwrapParam GetUnwrapParam(pb_UnwrapParameters parameters)
+		{
+			UnwrapParam param;
+			UnwrapParam.SetDefaults(out param);
 
-	/**
-	 * Store the previous GIWorkflowMode and set the current value to OnDemand (or leave it Legacy).
-	 */
-	[System.Diagnostics.Conditional("UNITY_5")]
-	internal static void PushGIWorkflowMode()
-	{
+			if(parameters != null)
+			{
+				param.angleError = Mathf.Clamp(parameters.angleError, 1f, 75f) * .01f;
+				param.areaError  = Mathf.Clamp(parameters.areaError , 1f, 75f) * .01f;
+				param.hardAngle  = Mathf.Clamp(parameters.hardAngle , 0f, 180f);
+				param.packMargin = Mathf.Clamp(parameters.packMargin, 1f, 64) * .001f;
+			}
+
+			return param;
+		}
+
+		/**
+		 * Store the previous GIWorkflowMode and set the current value to OnDemand (or leave it Legacy).
+		 */
+		[System.Diagnostics.Conditional("UNITY_5")]
+		internal static void PushGIWorkflowMode()
+		{
 #if UNITY_5
-		pb_Preferences_Internal.SetInt("pb_GIWorkflowMode", (int)Lightmapping.giWorkflowMode);
+			pb_Preferences_Internal.SetInt("pb_GIWorkflowMode", (int)Lightmapping.giWorkflowMode);
 
-		if(Lightmapping.giWorkflowMode != Lightmapping.GIWorkflowMode.Legacy)
-			Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+			if(Lightmapping.giWorkflowMode != Lightmapping.GIWorkflowMode.Legacy)
+				Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
 #endif
-	}
+		}
 
-	/**
-	 * Return GIWorkflowMode to it's prior state.
-	 */
-	[System.Diagnostics.Conditional("UNITY_5")]
-	internal static void PopGIWorkflowMode()
-	{
+		/**
+		 * Return GIWorkflowMode to it's prior state.
+		 */
+		[System.Diagnostics.Conditional("UNITY_5")]
+		internal static void PopGIWorkflowMode()
+		{
 #if UNITY_5
-		// if no key found (?), don't do anything.
-		if(!pb_Preferences_Internal.HasKey("pb_GIWorkflowMode"))
-			return;
+			// if no key found (?), don't do anything.
+			if(!pb_Preferences_Internal.HasKey("pb_GIWorkflowMode"))
+				return;
 
-		 Lightmapping.giWorkflowMode = (Lightmapping.GIWorkflowMode)pb_Preferences_Internal.GetInt("pb_GIWorkflowMode");
+			 Lightmapping.giWorkflowMode = (Lightmapping.GIWorkflowMode)pb_Preferences_Internal.GetInt("pb_GIWorkflowMode");
 #endif
+		}
 	}
 }
