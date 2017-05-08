a6fd32f7d990d7db1f8f86f97e7af9a009e2f9ab
karl- <karlh07@gmail.com>
2017-05-08 14:54:57 -0400

    Clarify git status warning in advanced build script

--- advanced.sh
+++ advanced.sh
@@ -4,6 +4,7 @@ if [[ -z $(git status -s) ]]
 then
 	mono pb-build.exe build/targets/ProBuilderAdvanced-5.5.json -debug
 else
-	echo "uncommitted changes in git, bailing out"
+	echo "Uncommitted changes in git, bailing out."
+	echo "Please commit or stash your changes before building."
 	exit 1
 fi
