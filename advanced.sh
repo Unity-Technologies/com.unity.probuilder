c87e448e1c04b2f32914123315620e57081ff7f5
karl- <karlh07@gmail.com>
2017-05-15 11:13:01 -0400

    Include 5.5 in build targets, and don't target debug

--- advanced.sh
+++ advanced.sh
@@ -3,13 +3,15 @@
 if [[ -z $(git status -s) ]]
 then
 echo Build 4.7
-	mono pb-build.exe build/targets/ProBuilderAdvanced-4.7.json -debug
+	mono pb-build.exe build/targets/ProBuilderAdvanced-4.7.json
 echo Build 5.0
-	mono pb-build.exe build/targets/ProBuilderAdvanced-5.0.json -debug
+	mono pb-build.exe build/targets/ProBuilderAdvanced-5.0.json
 echo Build 5.3
-	mono pb-build.exe build/targets/ProBuilderAdvanced-5.3.json -debug
+	mono pb-build.exe build/targets/ProBuilderAdvanced-5.3.json
+echo Build 5.5
+	mono pb-build.exe build/targets/ProBuilderAdvanced-5.5.json
 echo Build 5.6
-	mono pb-build.exe build/targets/ProBuilderAdvanced-5.6.json -debug
+	mono pb-build.exe build/targets/ProBuilderAdvanced-5.6.json
 else
 	echo "Uncommitted changes in git, bailing out."
 	echo "Please commit or stash your changes before building."
