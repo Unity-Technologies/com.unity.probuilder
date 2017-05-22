822a837007d259ec0f50ca7f6907628f51f82d0b
karl- <karlh07@gmail.com>
2017-05-22 15:13:31 -0400

    Make generating the current.txt file a part of building site

--- docs/build-site.sh
+++ docs/build-site.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+set -e
+
 cp ../probuilder2.0/Assets/ProCore/ProBuilder/About/changelog.txt docs/changelog.md
 
 mkdocs build -c
 
 find site/ -name '*.psd' -exec rm {} \;
 
+# generate the current.txt file
+mono gen_cur_txt.exe -f docs/changelog.md > site/current.txt
+
 SITE=../../site/probuilder2
 MKBUILD=$(pwd)
 
 cd $SITE
+
+git reset
+git checkout .
+git clean -df
 git pull origin gh-pages
 git rm -r ./*
 echo $MKBUILD/site/
