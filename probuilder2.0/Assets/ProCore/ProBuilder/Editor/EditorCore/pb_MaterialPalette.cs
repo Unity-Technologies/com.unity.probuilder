79de78b232ccf3ae09fef83e3783fdb01393c2de
karl- <karlh07@gmail.com>
2017-07-10 15:20:46 -0400

    Add a context menu item to quickly create material palettes from the current Asset window selection re #331

--- probuilder2.0/Assets/ProCore/ProBuilder/Editor/EditorCore/pb_MaterialPalette.cs
+++ probuilder2.0/Assets/ProCore/ProBuilder/Editor/EditorCore/pb_MaterialPalette.cs
@@ -1,15 +1,32 @@
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 using System.Collections;
 using ProBuilder2.Common;
 
 namespace ProBuilder2.EditorCommon
 {
 	/**
-	 *	A serializable object that stores an array of materials. Used by pb_Material_Editor.
+	 *	A serializable object that stores an array of materials. Used by pb_MaterialEditor.
 	 */
 	public class pb_MaterialPalette : ScriptableObject, pb_IHasDefault
 	{
+		[MenuItem("Assets/Create/Material Palette", true)]
+		static bool VerifyCreateMaterialPalette()
+		{
+			return Selection.GetFiltered(typeof(Material), SelectionMode.DeepAssets).Length > 0;
+		}
+
+		[MenuItem("Assets/Create/Material Palette")]
+		static void CreateMaterialPalette()
+		{
+			string path = AssetDatabase.GenerateUniqueAssetPath(pb_FileUtil.PathFromRelative("Data/Material Palette.asset"));
+			pb_MaterialPalette newPalette = pb_FileUtil.LoadRequired<pb_MaterialPalette>(path);
+			newPalette.array = Selection.GetFiltered(typeof(Material), SelectionMode.DeepAssets).Cast<Material>().ToArray();
+			EditorUtility.SetDirty(newPalette);
+			EditorGUIUtility.PingObject(newPalette);
+		}
+
 		public Material[] array;
 
 		public static implicit operator Material[](pb_MaterialPalette materialArray)
