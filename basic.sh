4d737ad059f9a3248a20d369df760af211b85d7f
karl- <karlh07@gmail.com>
2017-07-13 13:51:02 -0400

    Build basic targets in one command instead of 4

--- basic.sh
+++ basic.sh
@@ -2,14 +2,7 @@
 
 if [[ -z $(git status -s) ]]
 then
-echo Build 5.0
-	mono pb-build.exe build/targets/ProBuilderBasic-5.0.json
-echo Build 5.3
-	mono pb-build.exe build/targets/ProBuilderBasic-5.3.json
-echo Build 5.5
-	mono pb-build.exe build/targets/ProBuilderBasic-5.5.json
-echo Build 5.6
-	mono pb-build.exe build/targets/ProBuilderBasic-5.6.json
+	mono pb-build.exe build/targets/ProBuilderBasic-5.0.json build/targets/ProBuilderBasic-5.3.json build/targets/ProBuilderBasic-5.5.json build/targets/ProBuilderBasic-5.6.json
 else
 	echo "Uncommitted changes in git, bailing out."
 	echo "Please commit or stash your changes before building."
