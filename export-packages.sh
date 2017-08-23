911e3425a9d09324e4f2949263984c2901e8ba29
Karl Henkel <karlh07@gmail.com>
2017-08-23 16:59:06 -0400

    Don't build 4.7 and 5.0 targets anymore

--- export-packages.sh
+++ export-packages.sh
@@ -4,8 +4,6 @@
 # If no arguments are passed all targets are built.
 # Valid arguments:
 # 	SRC = Build source code package
-# 	47 = Build Unity 4.7 package
-# 	50 = Build Unity 5.0 package
 # 	53 = Build Unity 5.3 package
 # 	55 = Build Unity 5.5 package
 # 	56 = Build Unity 5.6 package
@@ -16,8 +14,6 @@ ARGC=${#ARGS[@]}
 
 if [ "$(uname)" == "Darwin" ]; then
 WORKING_DIR=$(pwd)
-UNITY_47=/Applications/Unity\ 4.7/Unity.app/Contents/MacOS/Unity
-UNITY_50=/Applications/Unity\ 5.0/Unity.app/Contents/MacOS/Unity
 UNITY_53=/Applications/Unity\ 5.3/Unity.app/Contents/MacOS/Unity
 UNITY_55=/Applications/Unity\ 5.5/Unity.app/Contents/MacOS/Unity
 UNITY_56=/Applications/Unity\ 5.6/Unity.app/Contents/MacOS/Unity
@@ -25,16 +21,14 @@ UNITY_2017_2=/Applications/Unity\ 2017.2/Unity.app/Contents/MacOS/Unity
 else
 # cygwin paths don't cut it in -projectPath
 WORKING_DIR=$(cygpath -aw $(pwd))
-UNITY_47=/d/Applications/Unity\ 4.7.0f1/Editor/Unity.exe
-UNITY_50=/d/Applications/Unity\ 5.0.0f4/Editor/Unity.exe
 UNITY_53=/d/Applications/Unity\ 5.3.0f4/Editor/Unity.exe
 UNITY_55=/d/Applications/Unity\ 5.5.0f3/Editor/Unity.exe
 UNITY_56=/d/Applications/Unity\ 5.6.0f3/Editor/Unity.exe
 UNITY_2017_2=/d/Applications/Unity\ 2017.2.0b2/Editor/Unity.exe
 fi
 
-UNITY_TARGET_MACRO=(UNITY_47 UNITY_47 UNITY_50 UNITY_53 UNITY_55 UNITY_56 UNITY_2017_2)
-UNITY_TARGET_SUFFIX=(SRC 47 50 53 55 56 2017.2)
+UNITY_TARGET_MACRO=(UNITY_53 UNITY_53 UNITY_55 UNITY_56 UNITY_2017_2)
+UNITY_TARGET_SUFFIX=(SRC 53 55 56 2017.2)
 UNITY_TARGETS_COUNT=${#UNITY_TARGET_MACRO[@]}
 
 VERSION_LINE=$(grep version: $WORKING_DIR/probuilder2.0/Assets/ProCore/ProBuilder/About/pc_AboutEntry_ProBuilder.txt)
