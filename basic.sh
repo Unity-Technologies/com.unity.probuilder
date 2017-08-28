52df7d06a3f64ec97be51ee94760b42967923274
Karl Henkel <karlh07@gmail.com>
2017-08-28 12:42:24 -0400

    Remove 4.7 and 5.0 targets from build scripts

--- basic.sh
+++ basic.sh
@@ -2,7 +2,7 @@
 
 if [[ -z $(git status -s) ]]
 then
-	mono pb-build.exe build/targets/ProBuilderBasic-5.0.json build/targets/ProBuilderBasic-5.3.json build/targets/ProBuilderBasic-5.5.json build/targets/ProBuilderBasic-5.6.json
+	mono pb-build.exe build/targets/ProBuilderBasic-5.3.json build/targets/ProBuilderBasic-5.5.json build/targets/ProBuilderBasic-5.6.json
 else
 	echo "Uncommitted changes in git, bailing out."
 	echo "Please commit or stash your changes before building."
--- export-packages.sh
+++ export-packages.sh
@@ -4,8 +4,8 @@
 # If no arguments are passed all targets are built.
 # Valid arguments:
 # 	SRC = Build source code package
-# 	47 = Build Unity 4.7 package
-# 	50 = Build Unity 5.0 package
+# 	47 = Build Unity 4.7 package (deprecated)
+# 	50 = Build Unity 5.0 package (deprecated)
 # 	53 = Build Unity 5.3 package
 # 	55 = Build Unity 5.5 package
 # 	56 = Build Unity 5.6 package
@@ -33,8 +33,8 @@ UNITY_56=/d/Applications/Unity\ 5.6.0f3/Editor/Unity.exe
 UNITY_2017_2=/d/Applications/Unity\ 2017.2.0b2/Editor/Unity.exe
 fi
 
-UNITY_TARGET_MACRO=(UNITY_47 UNITY_47 UNITY_50 UNITY_53 UNITY_55 UNITY_56 UNITY_2017_2)
-UNITY_TARGET_SUFFIX=(SRC 47 50 53 55 56 2017.2)
+UNITY_TARGET_MACRO=(UNITY_53 UNITY_53 UNITY_55 UNITY_56 UNITY_2017_2)
+UNITY_TARGET_SUFFIX=(SRC 53 55 56 2017.2)
 UNITY_TARGETS_COUNT=${#UNITY_TARGET_MACRO[@]}
 
 VERSION_LINE=$(grep version: $WORKING_DIR/probuilder2.0/Assets/ProCore/ProBuilder/About/pc_AboutEntry_ProBuilder.txt)
