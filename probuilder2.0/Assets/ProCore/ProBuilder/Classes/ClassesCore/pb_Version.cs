a83b27ee5eb315cc33e21929a3aced698e62274b
Karl Henkel <karlh07@gmail.com>
2017-12-07 16:43:45 -0500

    bump changelog

--- probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_Version.cs
+++ probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_Version.cs
@@ -4,10 +4,10 @@ namespace ProBuilder.Core
 {
 	public static class pb_Version
 	{
-#if PROBUILDER_RELEASE
-		public static readonly pb_VersionInfo VersionInfo = new pb_VersionInfo(2, 10, 0, 0, VersionType.Final, "");
+#if DEBUG
+		public static readonly pb_VersionInfo VersionInfo = new pb_VersionInfo(2, 10, 1, 0, VersionType.Development);
 #else
-		public static readonly pb_VersionInfo VersionInfo = new pb_VersionInfo(2, 10, 0, 0, VersionType.Development);
+		public static readonly pb_VersionInfo VersionInfo = new pb_VersionInfo(2, 10, 0, 0, VersionType.Final, "12/7/2017");
 #endif
 	}
 }
