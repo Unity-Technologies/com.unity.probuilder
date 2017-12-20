e9dc2a3600cec86a4f1a72e87aab7084745eac86
Karl Henkel <karlh07@gmail.com>
2017-12-20 14:55:09 -0500

    Update changes with known issues

--- probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_Version.cs
+++ probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_Version.cs
@@ -47,7 +47,7 @@ namespace ProBuilder.Core
 			}
 		}
 #else
-		public static readonly pb_VersionInfo Current = new pb_VersionInfo("2.10.1f0", "en-US: 1-1-2017");
+		public static readonly pb_VersionInfo Current = new pb_VersionInfo("2.10.0f0", "en-US: 1-1-2017");
 #endif
 	}
 }
