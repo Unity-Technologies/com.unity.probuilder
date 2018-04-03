140184dca95e6f0f4b61eadce4b3a056602cb7c7
Karl Henkel <karlh@unity3d.com>
2018-04-03 15:44:34 -0400

    Bump unwrap padding up to 6 to appease the progressive lightmapping overlap checker

--- probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_UnwrapParameters.cs
+++ probuilder2.0/Assets/ProCore/ProBuilder/Classes/ClassesCore/pb_UnwrapParameters.cs
@@ -12,25 +12,30 @@ namespace ProBuilder.Core
 		/// Angle between neighbor triangles that will generate seam.
 		/// </summary>
 		[Tooltip("Angle between neighbor triangles that will generate seam.")]
-		[Range(1f, 180f)] public float hardAngle = 88f;
+		[Range(1f, 180f)]
+		public float hardAngle = 88f;
 
 		/// <summary>
 		/// Measured in pixels, assuming mesh will cover an entire 1024x1024 lightmap.
 		/// </summary>
 		[Tooltip("Measured in pixels, assuming mesh will cover an entire 1024x1024 lightmap.")]
-		[Range(1f, 64f)] public float packMargin = 4f;
+		[Range(1f, 64f)]
+		public float packMargin = 6f;
 
 		/// <summary>
 		/// Measured in percents. Angle error measures deviation of UV angles from geometry angles. Area error measures deviation of UV triangles area from geometry triangles if they were uniformly scaled.
 		/// </summary>
-		[Tooltip("Measured in percents. Angle error measures deviation of UV angles from geometry angles. Area error measures deviation of UV triangles area from geometry triangles if they were uniformly scaled.")]
-		[Range(1f, 75f)] public float angleError = 8f;
+		[Tooltip(
+			"Measured in percents. Angle error measures deviation of UV angles from geometry angles. Area error measures deviation of UV triangles area from geometry triangles if they were uniformly scaled.")]
+		[Range(1f, 75f)]
+		public float angleError = 8f;
 
 		/// <summary>
 		/// Does... something.
 		/// </summary>
 		[Tooltip("Does... something.")]
-		[Range(1f, 75f)] public float areaError = 15f;
+		[Range(1f, 75f)]
+		public float areaError = 15f;
 
 		/// <summary>
 		/// Reset the unwrap parameters to default values.
@@ -38,7 +43,7 @@ namespace ProBuilder.Core
 		public void Reset()
 		{
 			hardAngle 	= 88f;
-			packMargin 	= 4f;
+			packMargin 	= 6f;
 			angleError 	= 8f;
 			areaError 	= 15f;
 		}
