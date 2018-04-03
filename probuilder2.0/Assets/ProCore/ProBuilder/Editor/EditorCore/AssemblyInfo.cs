34f964dfd421d3abc2bfe3c58836c404bb369d03
Karl Henkel <karlh@unity3d.com>
2018-04-03 13:28:27 -0400

    Reorganize project into packman format

--- probuilder2.0/Assets/ProCore/ProBuilder/Editor/EditorCore/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿#if PROBUILDER_DLL
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.AddOns.Editor")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.Tests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.Editor.Tests")]
-#else
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.AddOns.Editor")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.Tests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.Editor.Tests")]
-#endif
