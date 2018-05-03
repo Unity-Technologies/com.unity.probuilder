a448c0649278e51f71f71b1b385666ad2275a76d
Karl Henkel <karlh@unity3d.com>
2018-05-03 16:55:18 -0400

    Remove errant using statement

--- Runtime/Core/ProBuilderMesh.cs
+++ Runtime/Core/ProBuilderMesh.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using UnityEngine.Serialization;
 using System;
 using System.Collections.ObjectModel;
-using FlyingWormConsole3.FullSerializer.Internal;
 
 namespace UnityEngine.ProBuilder
 {
