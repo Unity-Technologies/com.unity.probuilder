4e5469690cc2b5530e3031cbb7da4de15985cfcf
Karl Henkel <karlh@unity3d.com>
2018-07-09 14:31:05 -0400

    add upgrade projects

---
+++ UpgradeProjects/3.0.8/Assets/TestScene/PBScriptTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PBScriptTest : MonoBehaviour {
+	
+	//the object to animate when this trigger is hit
+	public Animation animationTarget;
+	
+	//when this trigger is hit...
+	void OnTriggerEnter () {
+		//...make an object animate
+		animationTarget.Play();
+	}
+}
