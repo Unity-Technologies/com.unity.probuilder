79d06794009efde5fd5c0b9667f568b971c25d87
Karl Henkel <karlh@unity3d.com>
2018-09-11 17:10:41 -0400

    add a new settings class

---
+++ com.unity.probuilder/Debug/Editor/EditorPrefViewer.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorPrefBrowser : EditorWindow
+{
+	private static class Styles
+	{
+		public static readonly GUIStyle ToolbarSearchField = "ToolbarSeachTextField";
+		public static readonly GUIStyle ToolbarSearchFieldCancel = "ToolbarSeachCancelButton";
+		public static readonly GUIStyle ToolbarSearchFieldCancelEmpty = "ToolbarSeachCancelButtonEmpty";
+
+		public static readonly GUIStyle HeaderBackground = new GUIStyle(GUI.skin.box);
+
+		static Styles()
+		{
+			// Zero out margin to go to edges of window
+			HeaderBackground.margin = new RectOffset();
+
+			//
+			HeaderBackground.overflow = new RectOffset(1, 1, 0, 0);
+		}
+	}
+
+	private const string kUnityRootSubKey = "Software\\Unity Technologies\\Unity Editor 5.x\\";
+
+	[NonSerialized]
+	private readonly SortedDictionary<string, object> m_EditorPrefsLookup = new SortedDictionary<string, object>();
+
+	[SerializeField]
+	private Vector2 m_ScrollPosition = new Vector2(0f, 0f);
+
+	[SerializeField]
+	private string m_Filter = "";
+
+	private bool IsFiltering
+	{
+		get { return !string.IsNullOrEmpty(m_Filter); }
+	}
+
+	[MenuItem("Window/Editor Pref Browser")]
+	public static void ShowWindow()
+	{
+		GetWindow<EditorPrefBrowser>().titleContent = new GUIContent("Editor Pref");
+	}
+
+	public void OnEnable()
+	{
+		FetchKeyValues();
+	}
+
+	public void OnGUI()
+	{
+		EditorGUIUtility.labelWidth = 400f;
+		DoToolbar();
+		DoHeader();
+		DoList();
+		EditorGUIUtility.labelWidth = 0f;
+	}
+
+	private void DoHeader()
+	{
+		using (new EditorGUILayout.HorizontalScope(Styles.HeaderBackground, GUILayout.ExpandHeight(false)))
+		{
+			GUILayout.Label("Name", GUILayout.Width(EditorGUIUtility.labelWidth));
+			GUILayout.Label("Value");
+		}
+	}
+
+	private void DoList()
+	{
+		using (var scrollView = new EditorGUILayout.ScrollViewScope(m_ScrollPosition))
+		{
+			m_ScrollPosition = scrollView.scrollPosition;
+
+			EditorGUI.BeginChangeCheck();
+			string valueName = null;
+			object value = null;
+
+			foreach (var kvp in m_EditorPrefsLookup)
+			{
+				valueName = kvp.Key;
+				value = kvp.Value;
+
+				if (IsFiltering && !valueName.ToLower().Contains(m_Filter.ToLower()))
+					continue;
+
+				// Strings are encoded as utf8 bytes
+				var bytes = value as byte[];
+				if (bytes != null)
+				{
+					string valueAsString = Encoding.UTF8.GetString(bytes);
+					EditorGUI.BeginChangeCheck();
+					string newString = EditorGUILayout.DelayedTextField(StripValueNameHash(valueName), valueAsString);
+					if (EditorGUI.EndChangeCheck())
+					{
+						value = Encoding.UTF8.GetBytes(newString);
+						break;
+					}
+				}
+				else if (value is int)
+				{
+					int valueAsInt = (int)value;
+					EditorGUI.BeginChangeCheck();
+					int newInt = EditorGUILayout.DelayedIntField(StripValueNameHash(valueName), valueAsInt);
+					if (EditorGUI.EndChangeCheck())
+					{
+						value = newInt;
+						break;
+					}
+				}
+				else
+				{
+					EditorGUILayout.LabelField(StripValueNameHash(valueName), string.Format("Unhandled Type {0}", value.GetType()));
+				}
+			}
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				SetKeyValue(valueName, value);
+			}
+		}
+	}
+
+	private void SetKeyValue(string valueName, object newValue)
+	{
+		if (valueName == null)
+			throw new ArgumentNullException("valueName");
+
+		if (newValue == null)
+			throw new ArgumentNullException("newValue");
+
+		using (RegistryKey key = Registry.CurrentUser.OpenSubKey(kUnityRootSubKey, true))
+		{
+			if (key == null)
+				throw new KeyNotFoundException(string.Format("Failed to open sub key {0}.", kUnityRootSubKey));
+
+			// Unity caches values, so it doesn't dip into the registry for every EditorPrefs.Get* call.
+			// This means we need to tell Unity to delete this value to remove it from the cache and force Unity to look into registry for value.
+			EditorPrefs.DeleteKey(StripValueNameHash(valueName));
+
+			key.SetValue(valueName, newValue);
+			m_EditorPrefsLookup[valueName] = key.GetValue(valueName);
+		}
+	}
+
+	private void DoToolbar()
+	{
+		using (new EditorGUILayout.HorizontalScope(EditorStyles.toolbar))
+		{
+			// Refresh Button
+			if (GUILayout.Button("Refresh", EditorStyles.toolbarButton))
+				FetchKeyValues();
+
+			GUILayout.FlexibleSpace();
+
+			// Filter Field
+			m_Filter = EditorGUILayout.TextField(m_Filter, Styles.ToolbarSearchField, GUILayout.Width(250f));
+			if (GUILayout.Button(GUIContent.none, IsFiltering ? Styles.ToolbarSearchFieldCancel : Styles.ToolbarSearchFieldCancelEmpty))
+			{
+				m_Filter = "";
+				GUIUtility.keyboardControl = 0;
+			}
+		}
+	}
+
+	private void FetchKeyValues()
+	{
+		using (RegistryKey key = Registry.CurrentUser.OpenSubKey(kUnityRootSubKey, false))
+		{
+			if (key == null)
+				throw new KeyNotFoundException(string.Format("Failed to open sub key {0}.", kUnityRootSubKey));
+
+			m_EditorPrefsLookup.Clear();
+
+			foreach (string keyValueName in key.GetValueNames())
+			{
+				var value = key.GetValue(keyValueName);
+				m_EditorPrefsLookup.Add(keyValueName, value);
+			}
+		}
+	}
+
+	private string StripValueNameHash(string keyValueName)
+	{
+		return keyValueName.Split(new[] { "_h" }, StringSplitOptions.None).First();
+	}
+}
--- com.unity.probuilder/Editor/EditorCore/Settings.cs
+++ com.unity.probuilder/Editor/EditorCore/Settings.cs
@@ -1,29 +1,58 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 
 namespace UnityEngine.ProBuilder
 {
-	sealed class SettingsDictionary
+	[Serializable]
+	sealed class ValueWrapper<T>
 	{
-		[Serializable]
-		struct SerializableWrapper<T>
-		{
-			[SerializeField]
-			public T value;
+#if PRETTY_PRINT_JSON
+		const bool k_PrettyPrintJson = true;
+#else
+		const bool k_PrettyPrintJson = false;
+#endif
 
-			public SerializableWrapper(T value)
-			{
-				this.value = value;
-			}
+		[SerializeField]
+		T m_Value;
+
+		public static string Serialize(T value)
+		{
+			var obj = new ValueWrapper<T>() { m_Value = value };
+			return EditorJsonUtility.ToJson(obj, k_PrettyPrintJson);
 		}
 
-		Dictionary<string, Dictionary<string, string>> m_Dictionary = new Dictionary<string, Dictionary<string, string>>();
+		public static T Deserialize(string json)
+		{
+			var value = (object)Activator.CreateInstance<ValueWrapper<T>>();
+			EditorJsonUtility.FromJsonOverwrite(json, value);
+			return ((ValueWrapper<T>)value).m_Value;
+		}
+	}
+
+	[Serializable]
+	sealed class SettingsDictionary : ISerializationCallbackReceiver
+	{
+		[Serializable]
+		struct SettingsKeyValuePair
+		{
+			public string type;
+			public string key;
+			public string value;
+		}
+
+#pragma warning disable 0649
+		[SerializeField]
+		List<SettingsKeyValuePair> m_DictionaryValues;
+#pragma warning restore 0649
+
+		internal Dictionary<string, Dictionary<string, string>> dictionary = new Dictionary<string, Dictionary<string, string>>();
 
 		public bool ContainsKey<T>(string key)
 		{
 			var type = typeof(T).AssemblyQualifiedName;
-			return m_Dictionary.ContainsKey(type) && m_Dictionary[type].ContainsKey(key);
+			return dictionary.ContainsKey(type) && dictionary[type].ContainsKey(key);
 		}
 
 		public void Set<T>(string key, T value)
@@ -33,14 +62,20 @@ namespace UnityEngine.ProBuilder
 
 			var type = typeof(T).AssemblyQualifiedName;
 
+			Set(type, key, ValueWrapper<T>.Serialize(value));
+		}
+
+		public void Set(string type, string key, string value)
+		{
 			Dictionary<string, string> entries;
 
-			if (!m_Dictionary.TryGetValue(type, out entries))
-				m_Dictionary.Add(type, entries = new Dictionary<string, string>());
+			if (!dictionary.TryGetValue(type, out entries))
+				dictionary.Add(type, entries = new Dictionary<string, string>());
+
 			if (entries.ContainsKey(key))
-				entries[key] = EditorJsonUtility.ToJson(new SerializableWrapper<T>(value));
+				entries[key] = value;
 			else
-				entries.Add(key, EditorJsonUtility.ToJson(new SerializableWrapper<T>(value)));
+				entries.Add(key, value);
 		}
 
 		public T Get<T>(string key, T fallback = default)
@@ -51,14 +86,11 @@ namespace UnityEngine.ProBuilder
 			var type = typeof(T).AssemblyQualifiedName;
 			Dictionary<string, string> entries;
 
-			if (m_Dictionary.TryGetValue(type, out entries) && entries.ContainsKey(key))
+			if (dictionary.TryGetValue(type, out entries) && entries.ContainsKey(key))
 			{
 				try
 				{
-					// type must be boxed for FromJsonOverwrite to correctly populate object
-					var value = (object) new SerializableWrapper<T>(Activator.CreateInstance<T>());
-					EditorJsonUtility.FromJsonOverwrite(entries[key], value);
-					return ((SerializableWrapper<T>)value).value;
+					return ValueWrapper<T>.Deserialize(entries[key]);
 				}
 				catch
 				{
@@ -68,10 +100,214 @@ namespace UnityEngine.ProBuilder
 
 			return fallback;
 		}
+
+		public void OnBeforeSerialize()
+		{
+			m_DictionaryValues.Clear();
+
+			foreach (var type in dictionary)
+			{
+				foreach (var entry in type.Value)
+				{
+					m_DictionaryValues.Add(new SettingsKeyValuePair()
+					{
+						type = type.Key,
+						key = entry.Key,
+						value = entry.Value
+					});
+				}
+			}
+		}
+
+		public void OnAfterDeserialize()
+		{
+			dictionary.Clear();
+
+			foreach (var entry in m_DictionaryValues)
+			{
+				Dictionary<string, string> entries;
+
+				if (dictionary.TryGetValue(entry.type, out entries))
+					entries.Add(entry.key, entry.value);
+				else
+					dictionary.Add(entry.type, new Dictionary<string, string>() { { entry.key, entry.value } } );
+			}
+		}
 	}
 
-	public class Settings
+	sealed class Pref<T>
 	{
+		bool m_Initialized;
+		string m_Key;
+		T m_Value;
+		Settings.Scope m_Scope;
 
+		public Pref(string key, T value, Settings.Scope scope = Settings.Scope.Project)
+		{
+			m_Key = key;
+			m_Value = value;
+			m_Scope = scope;
+		}
+
+		public string key
+		{
+			get { return m_Key; }
+		}
+
+		public T value
+		{
+			get
+			{
+				if (!m_Initialized)
+				{
+					m_Initialized = true;
+
+					if(Settings.ContainsKey<T>(m_Key, m_Scope))
+						m_Value = Settings.Get<T>(m_Key, m_Scope);
+				}
+
+				return m_Value;
+			}
+
+			set
+			{
+				if (Equals(m_Value, value))
+					return;
+
+				m_Value = value;
+				Settings.Set<T>(key, m_Value, m_Scope);
+			}
+		}
+
+		public static implicit operator T(Pref<T> pref)
+		{
+			return pref.value;
+		}
+	}
+
+	[Serializable]
+	sealed class Settings
+	{
+		public enum Scope
+		{
+			Project,
+			Global
+		}
+
+		const string k_SettingsPath = "ProjectSettings/ProBuilderSettings.json";
+
+#if PRETTY_PRINT_JSON
+		const bool k_PrettyPrintJson = true;
+#else
+		const bool k_PrettyPrintJson = false;
+#endif
+
+		static Settings s_Instance;
+
+		[SerializeField]
+		SettingsDictionary m_Dictionary = new SettingsDictionary();
+
+		static Settings instance
+		{
+			get
+			{
+				if (s_Instance == null)
+				{
+					s_Instance = new Settings();
+					Load();
+				}
+
+				return s_Instance;
+			}
+		}
+
+		Settings() { }
+
+		public static void Save()
+		{
+			File.WriteAllText(k_SettingsPath, EditorJsonUtility.ToJson(instance, k_PrettyPrintJson));
+		}
+
+		public static void Load()
+		{
+			if (File.Exists(k_SettingsPath))
+			{
+				var json = File.ReadAllText(k_SettingsPath);
+				EditorJsonUtility.FromJsonOverwrite(json, instance);
+			}
+		}
+
+		internal static SettingsDictionary dictionary
+		{
+			get { return instance.m_Dictionary; }
+		}
+
+		static string GetEditorPrefKey<T>(string key)
+		{
+			return GetEditorPrefKey(typeof(T).AssemblyQualifiedName, key);
+		}
+
+		static string GetEditorPrefKey(string assemblyQualifiedTypeName, string key)
+		{
+			return "ProBuilder::" + assemblyQualifiedTypeName + "::" + key;
+		}
+
+		static void SetEditorPref<T>(string key, T value)
+		{
+			var k = GetEditorPrefKey<T>(key);
+			EditorPrefs.SetString(k, ValueWrapper<T>.Serialize(value));
+		}
+
+		static T GetEditorPref<T>(string key, T fallback = default)
+		{
+			var k = GetEditorPrefKey<T>(key);
+			if(EditorPrefs.HasKey(k))
+				return ValueWrapper<T>.Deserialize(EditorPrefs.GetString(k));
+			return fallback;
+		}
+
+		public static void Set<T>(string key, T value, Scope scope = Scope.Project)
+		{
+			switch (scope)
+			{
+				case Scope.Project:
+					instance.m_Dictionary.Set<T>(key, value);
+				break;
+
+				case Scope.Global:
+					SetEditorPref<T>(key, value);
+				break;
+			}
+		}
+
+		public static void Set(string type, string key, string json, Scope scope = Scope.Project)
+		{
+			switch (scope)
+			{
+				case Scope.Project:
+					instance.m_Dictionary.Set(type, key, json);
+					break;
+
+				case Scope.Global:
+					EditorPrefs.SetString(GetEditorPrefKey(type, key), json);
+					break;
+			}
+		}
+
+		public static T Get<T>(string key, Scope scope = Scope.Project, T fallback = default)
+		{
+			if(scope == Scope.Project)
+				return instance.m_Dictionary.Get<T>(key, fallback);
+
+			return GetEditorPref(key, fallback);
+		}
+
+		public static bool ContainsKey<T>(string key, Scope scope = Scope.Project)
+		{
+			if(scope == Scope.Project)
+				return instance.m_Dictionary.ContainsKey<T>(key);
+
+			return EditorPrefs.HasKey(GetEditorPrefKey<T>(key));
+		}
 	}
 }
