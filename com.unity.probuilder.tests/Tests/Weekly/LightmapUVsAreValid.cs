2e3208eef6a8517b38b9d34e284244db24e242ec
Karl Henkel <karlh@unity3d.com>
2018-10-04 17:26:41 -0400

    run lightmap unit tests async, fixing instabilities on 2018.1/2. also fix compile errors on 2018.1 in meshutils

--- com.unity.probuilder.tests/Tests/Weekly/LightmapUVsAreValid.cs
+++ com.unity.probuilder.tests/Tests/Weekly/LightmapUVsAreValid.cs
@@ -1,7 +1,10 @@
 ﻿using System.Linq;
 using NUnit.Framework;
+using System.Collections;
+using System.IO;
 using UnityEditor;
 using UnityEditor.ProBuilder;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.TestTools;
 using Lightmapping = UnityEditor.Lightmapping;
@@ -9,31 +12,70 @@ using UnityEngine.ProBuilder.Tests.Framework;
 
 namespace UnityEngine.ProBuilder.Tests.Slow
 {
-	static class LightmapUVsAreValid
+	class LightmapUVsAreValid
 	{
-		[Test]
-		public static void DefaultUnwrapParamsDoNotOverlap()
+		const string k_LightmapUnitTestsDir = "Assets/LightmappingUnitTests";
+		const string k_LightmapUnitTestsScene = "Assets/LightmappingUnitTests/LightmappingUnitTestScene.unity";
+
+		static void Setup()
+		{
+			if (!Directory.Exists(k_LightmapUnitTestsDir))
+				Directory.CreateDirectory(k_LightmapUnitTestsDir);
+
+			AssetDatabase.Refresh();
+
+			var scene = SceneManagement.SceneManager.GetActiveScene();
+			EditorSceneManager.SaveScene(scene, k_LightmapUnitTestsScene, false);
+		}
+
+		static void Cleanup()
+		{
+			Directory.Delete(k_LightmapUnitTestsDir, true);
+			File.Delete(k_LightmapUnitTestsDir + ".meta");
+			AssetDatabase.Refresh();
+		}
+
+		bool s_FinishedBaking;
+
+		[UnityTest]
+		public IEnumerator DefaultUnwrapParamsDoNotOverlap()
 		{
 			var lightmapMode = Lightmapping.giWorkflowMode;
-			Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
-			float x = -10f;
 
-			using (var shapes = new TestUtility.BuiltInPrimitives())
+			try
 			{
-				SceneModeUtility.SetStaticFlags(shapes.Select(it => it.gameObject).ToArray(), (int) StaticEditorFlags.LightmapStatic, true);
+				Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+				Lightmapping.started += () => { s_FinishedBaking = false; };
+				Lightmapping.completed += () => { s_FinishedBaking = true; };
 
-				foreach (ProBuilderMesh mesh in shapes)
+				Setup();
+
+				float x = -10f;
+
+				using (var shapes = new TestUtility.BuiltInPrimitives())
 				{
-					mesh.transform.position = new Vector3(x, 0f, 0f);
-					x += mesh.mesh.bounds.size.x + .5f;
-					mesh.Optimize(true);
+					SceneModeUtility.SetStaticFlags(shapes.Select(it => it.gameObject).ToArray(), (int)StaticEditorFlags.LightmapStatic, true);
+
+					foreach (ProBuilderMesh mesh in shapes)
+					{
+						mesh.transform.position = new Vector3(x, 0f, 0f);
+						x += mesh.mesh.bounds.size.x + .5f;
+						mesh.Optimize(true);
+					}
+
+					Lightmapping.BakeAsync();
+
+					while (!s_FinishedBaking)
+						yield return null;
+
+					LogAssert.NoUnexpectedReceived();
 				}
-
-				Lightmapping.Bake();
-				LogAssert.NoUnexpectedReceived();
 			}
-
-			Lightmapping.giWorkflowMode = lightmapMode;
+			finally
+			{
+				Lightmapping.giWorkflowMode = lightmapMode;
+				Cleanup();
+			}
 		}
 	}
 }
