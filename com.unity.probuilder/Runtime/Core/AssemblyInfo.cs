76c08040a6942c0c37ff0350d02598a7e20f9f09
Karl Henkel <karlh07@gmail.com>
2018-12-14 11:52:30 -0500

    use asmdef optional dependencies for fbx addon

--- com.unity.probuilder/Runtime/Core/AssemblyInfo.cs
+++ com.unity.probuilder/Runtime/Core/AssemblyInfo.cs
@@ -4,3 +4,4 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.Tests.Slow")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.Editor.Tests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.Debug.Editor")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Unity.ProBuilder.AddOns.Editor")]
