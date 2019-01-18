c2357845b86ac98a7f28eae55f0dd206f2393071
Karl Henkel <karlh07@gmail.com>
2019-01-18 11:11:57 -0500

    4.0.2

--- com.unity.probuilder/Runtime/Core/Version.cs
+++ com.unity.probuilder/Runtime/Core/Version.cs
@@ -5,7 +5,7 @@ namespace UnityEngine.ProBuilder
     /// </summary>
     static class Version
     {
-        internal static readonly SemVer currentInfo = new SemVer("4.0.2-preview.0", "2019/01/18");
+        internal static readonly SemVer currentInfo = new SemVer("4.0.2", "2019/01/18");
 
         /// <summary>
         /// Get the current version.
