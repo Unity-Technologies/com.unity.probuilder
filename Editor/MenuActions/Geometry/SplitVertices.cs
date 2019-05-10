3d1989c3fd6356b513763f606b8ca2ee57379b7f
Karl Henkel <karlh@unity3d.com>
2019-05-10 14:49:55 -0400

    collect coincident vertices when doing split vertices action

--- Editor/MenuActions/Geometry/SplitVertices.cs
+++ Editor/MenuActions/Geometry/SplitVertices.cs
@@ -50,63 +50,17 @@ namespace UnityEditor.ProBuilder.Actions
 
             foreach (ProBuilderMesh mesh in MeshSelection.topInternal)
             {
-                // loose verts to split
-                List<int> tris = new List<int>(mesh.selectedIndexesInternal);
-
-                if (mesh.selectedFacesInternal.Length > 0)
-                {
-                    int[] sharedVertexHandles = new int[mesh.selectedIndexesInternal.Length];
-
-                    // Get shared index index for each vert in selection
-                    for (int i = 0; i < mesh.selectedIndexesInternal.Length; i++)
-                        sharedVertexHandles[i] = mesh.GetSharedVertexHandle(mesh.selectedIndexesInternal[i]);
-
-                    // cycle through selected faces and remove the tris that compose full faces.
-                    foreach (Face face in mesh.selectedFacesInternal)
-                    {
-                        List<int> faceSharedIndexes = new List<int>();
-
-                        for (int j = 0; j < face.distinctIndexesInternal.Length; j++)
-                            faceSharedIndexes.Add(mesh.GetSharedVertexHandle(face.distinctIndexesInternal[j]));
-
-                        List<int> usedTris = new List<int>();
-                        for (int i = 0; i < sharedVertexHandles.Length; i++)
-                            if (faceSharedIndexes.Contains(sharedVertexHandles[i]))
-                                usedTris.Add(mesh.selectedIndexesInternal[i]);
-
-                        // This face *is* composed of selected tris.  Remove these tris from the loose index list
-                        foreach (int i in usedTris)
-                            if (tris.Contains(i))
-                                tris.Remove(i);
-                    }
-                }
-
-                // Now split the faces, and any loose vertices
-                mesh.DetachFaces(mesh.selectedFacesInternal);
-
-                splitCount += mesh.selectedIndexesInternal.Length;
-                mesh.SplitVertices(mesh.selectedIndexesInternal);
-
-                // Reattach detached face vertices (if any are to be had)
-                if (mesh.selectedFacesInternal.Length > 0)
-                    mesh.WeldVertices(mesh.selectedFacesInternal.SelectMany(x => x.indexes), Mathf.Epsilon);
-
-                // And set the selected triangles to the newly split
-                List<int> newTriSelection = new List<int>(mesh.selectedFacesInternal.SelectMany(x => x.indexes));
-                newTriSelection.AddRange(tris);
-                mesh.SetSelectedVertices(newTriSelection.ToArray());
-
-                mesh.ToMesh();
-                mesh.Refresh();
-                mesh.Optimize();
+                var coincident = mesh.selectedCoincidentVertices;
+                splitCount += coincident.Count();
+                mesh.SplitVertices(coincident);
             }
 
             ProBuilderEditor.Refresh();
 
             if (splitCount > 0)
                 return new ActionResult(ActionResult.Status.Success, "Split " + splitCount + (splitCount > 1 ? " Vertices" : " Vertex"));
-            else
-                return new ActionResult(ActionResult.Status.Failure, "Split Vertices\nInsuffient Vertices Selected");
+                
+            return new ActionResult(ActionResult.Status.Failure, "Split Vertices\nInsuffient Vertices Selected");
         }
     }
 }
