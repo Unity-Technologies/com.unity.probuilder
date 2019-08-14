01cf6b962036bd1cad86011a6d59d6c4148998f8
MartinImgSpc <martin@imgspc.com>
2019-08-14 14:21:31 -0400

    Update documentation

--- Runtime/MeshOperations/CombineMeshes.cs
+++ Runtime/MeshOperations/CombineMeshes.cs
@@ -26,10 +26,10 @@ namespace UnityEngine.ProBuilder.MeshOperations
 
         /// <summary>
         /// Merge a collection of <see cref="ProBuilderMesh"/> objects to as few meshes as possible. It will re-use the meshTarget object as the first
-        /// destination for the first 65535 vertices. If the sum of vertices is above 65535 it will generate new meshes unless there is a single mesh left in which
-        /// case it will append it to the return list.
+        /// destination for the first <see cref="ProBuilderMesh.maxVertexCount"/> -1 vertices. If the sum of vertices is above <see cref="ProBuilderMesh.maxVertexCount"/> - 1
+        /// it will generate new meshes unless there is a single mesh left in which case it will append it to the return list.
         /// </summary>
-        /// <param name="meshes">A collection of meshes to be merged.</param>
+        /// <param name="meshes">A collection of meshes to be merged. Note: it is expected that meshes includes meshTarget.</param>
         /// <param name="meshTarget">A mesh which will be used as the starting point for merging and which will be kept as reference/target.</param>
         /// <returns>
         /// A list of merged meshes. In most cases this will be a single mesh corresponding to meshTarget. However it can be multiple in cases
