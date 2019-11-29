6efaa6d7f784b44ba1c71c04d9d5c63b7b5fe44b
Karl Henkel <karlh@unity3d.com>
2019-11-29 15:56:33 -0500

    move runtime tests out of deeply nested namespaces

--- Tests/Runtime/MeshOps/AppendElementsTests.cs
+++ Tests/Runtime/MeshOps/AppendElementsTests.cs
@@ -1,155 +1,152 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UObject = UnityEngine.Object;
 using NUnit.Framework;
-using UnityEngine.ProBuilder.Tests.Framework;
+using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
+using Math = System.Math;
 
-namespace UnityEngine.ProBuilder.RuntimeTests.MeshOperations
+class AppendElementsTests
 {
-    class AppendElementsTests
+    GameObject m_GO;
+    PolyShape m_Poly;
+    ProBuilderMesh m_pb;
+
+    [SetUp]
+    public void SetUp()
     {
-        GameObject m_GO;
-        PolyShape m_Poly;
-        ProBuilderMesh m_pb;
+        m_GO = new GameObject();
+        m_Poly = m_GO.AddComponent<PolyShape>();
+        m_pb = m_GO.AddComponent<ProBuilderMesh>();
+    }
 
-        [SetUp]
-        public void SetUp()
+    [TearDown]
+    public void Cleanup()
+    {
+        UObject.DestroyImmediate(m_GO);
+    }
+
+    [Test]
+    public void CreateShapeFromPolygon_CreateMeshFailure_TriangulationMissingPoints()
+    {
+        // Test that creating the shape fails if the triangulation missed some of the points (invalid mesh)
+        m_Poly.m_Points.Add(new Vector3(0.9f, 0, 3.4f));
+        m_Poly.m_Points.Add(new Vector3(6.1f, 0, 5.7f));
+        m_Poly.m_Points.Add(new Vector3(-1.6f, 0, 8.8f));
+        m_Poly.m_Points.Add(new Vector3(7.4f, 0, -2.6f));
+
+        var result = m_Poly.CreateShapeFromPolygon();
+        Assert.That(result.status, Is.EqualTo(ActionResult.Status.Failure));
+        Assert.That(result.notification, Is.EqualTo("Triangulation missing points"));
+
+        // check that the mesh is cleared
+        Assert.That(m_pb.facesInternal.Length, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void CreateShapeFromPolygon_CreateMeshSuccess()
+    {
+        // Test that creating a valid shape gives a result of Success
+        m_Poly.m_Points.Add(new Vector3(0, 0, 0));
+        m_Poly.m_Points.Add(new Vector3(0, 0, 2));
+        m_Poly.m_Points.Add(new Vector3(2, 0, 2));
+        m_Poly.m_Points.Add(new Vector3(2, 0, 0));
+
+        var result = m_Poly.CreateShapeFromPolygon();
+        Assert.That(result.status, Is.EqualTo(ActionResult.Status.Success));
+    }
+
+    [Test]
+    public void CreateShapeFromPolygon_ExtrudeOrNot()
+    {
+        m_Poly.m_Points.Add(new Vector3(0,0,0));
+        m_Poly.m_Points.Add(new Vector3(0,0,2));
+        m_Poly.m_Points.Add(new Vector3(2,0,0));
+        m_Poly.extrude = 0.0f;
+        m_Poly.flipNormals = false;
+        m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
+
+        Assert.That(m_pb.faceCount, Is.EqualTo(1));
+        m_pb.Clear();
+        Assert.That(m_pb.faceCount, Is.EqualTo(0));
+        m_Poly.extrude = 1.0f;
+        m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
+        Assert.That(m_pb.faceCount, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void CreateShapeFromPolygon_NormalFacing()
+    {
+        m_Poly.m_Points.Add(new Vector3(0, 0, 0));
+        m_Poly.m_Points.Add(new Vector3(0, 0, 2));
+        m_Poly.m_Points.Add(new Vector3(2, 0, 0));
+        m_Poly.extrude = 0.0f;
+        m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
+        Vector3 nrm = UnityEngine.ProBuilder.Math.Normal(m_pb, m_pb.facesInternal[0]);
+        Assert.That(nrm, Is.EqualTo(Vector3.up));
+
+        m_pb.Clear();
+        //Changing the winding order should lead to a norml in the other direction
+        m_Poly.m_Points.Reverse();
+        m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
+        nrm = UnityEngine.ProBuilder.Math.Normal(m_pb, m_pb.facesInternal[0]);
+        Assert.That(nrm, Is.EqualTo(Vector3.up));
+    }
+
+    [Test]
+    public void CreateShapeFromPolygon_Holes_Success()
+    {
+        // Test that creating a valid shape with a hole gives a result of Success
+        m_Poly.m_Points.Add(new Vector3(0, 0, 0));
+        m_Poly.m_Points.Add(new Vector3(0, 0, 2));
+        m_Poly.m_Points.Add(new Vector3(2, 0, 2));
+        m_Poly.m_Points.Add(new Vector3(2, 0, 0));
+
+        Vector3[][] holes = new Vector3[2][];
+        holes[0] = new[]
         {
-            m_GO = new GameObject();
-            m_Poly = m_GO.AddComponent<PolyShape>();
-            m_pb = m_GO.AddComponent<ProBuilderMesh>();
-        }
+            new Vector3(0.1f, 0, 0.1f),
+            new Vector3(0.1f, 0, 1.9f),
+            new Vector3(0.9f, 0, 1.9f),
+            new Vector3(0.9f, 0, 0.1f)
+        };
 
-        [TearDown]
-        public void Cleanup()
+        holes[1] = new[]
         {
-            UObject.DestroyImmediate(m_GO);
-        }
+            new Vector3(1.1f, 0, 0.1f),
+            new Vector3(1.9f, 0, 0.1f),
+            new Vector3(1.9f, 0, 1.9f),
+        };
 
-        [Test]
-        public void CreateShapeFromPolygon_CreateMeshFailure_TriangulationMissingPoints()
+        var result = m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals, holes);
+        Assert.That(result.status, Is.EqualTo(ActionResult.Status.Success));
+    }
+
+    [Test]
+    public void CreateShapeFromPolygon_CreateMeshFailure_TooFewPointsInHole()
+    {
+        // Test that creating a shape with an invalid holes parameter does nothing and reports an error.
+        m_Poly.m_Points.Add(new Vector3(0, 0, 0));
+        m_Poly.m_Points.Add(new Vector3(0, 0, 2));
+        m_Poly.m_Points.Add(new Vector3(2, 0, 2));
+        m_Poly.m_Points.Add(new Vector3(2, 0, 0));
+
+        Vector3[][] holes = new Vector3[2][];
+        holes[0] = new[]
         {
-            // Test that creating the shape fails if the triangulation missed some of the points (invalid mesh)
-            m_Poly.m_Points.Add(new Vector3(0.9f, 0, 3.4f));
-            m_Poly.m_Points.Add(new Vector3(6.1f, 0, 5.7f));
-            m_Poly.m_Points.Add(new Vector3(-1.6f, 0, 8.8f));
-            m_Poly.m_Points.Add(new Vector3(7.4f, 0, -2.6f));
+            new Vector3(0.1f, 0, 0.1f),
+            new Vector3(0.1f, 0, 1.9f),
+            new Vector3(0.9f, 0, 1.9f),
+            new Vector3(0.9f, 0, 0.1f)
+        };
 
-            var result = m_Poly.CreateShapeFromPolygon();
-            Assert.That(result.status, Is.EqualTo(ActionResult.Status.Failure));
-            Assert.That(result.notification, Is.EqualTo("Triangulation missing points"));
-
-            // check that the mesh is cleared
-            Assert.That(m_pb.facesInternal.Length, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void CreateShapeFromPolygon_CreateMeshSuccess()
+        holes[1] = new[]
         {
-            // Test that creating a valid shape gives a result of Success
-            m_Poly.m_Points.Add(new Vector3(0, 0, 0));
-            m_Poly.m_Points.Add(new Vector3(0, 0, 2));
-            m_Poly.m_Points.Add(new Vector3(2, 0, 2));
-            m_Poly.m_Points.Add(new Vector3(2, 0, 0));
+            new Vector3(1.1f, 0, 0.1f),
+            new Vector3(1.9f, 0, 0.1f),
+        };
 
-            var result = m_Poly.CreateShapeFromPolygon();
-            Assert.That(result.status, Is.EqualTo(ActionResult.Status.Success));
-        }
-
-        [Test]
-        public void CreateShapeFromPolygon_ExtrudeOrNot()
-        {
-            m_Poly.m_Points.Add(new Vector3(0,0,0));
-            m_Poly.m_Points.Add(new Vector3(0,0,2));
-            m_Poly.m_Points.Add(new Vector3(2,0,0));
-            m_Poly.extrude = 0.0f;
-            m_Poly.flipNormals = false;
-            m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
-
-            Assert.That(m_pb.faceCount, Is.EqualTo(1));
-            m_pb.Clear();
-            Assert.That(m_pb.faceCount, Is.EqualTo(0));
-            m_Poly.extrude = 1.0f;
-            m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
-            Assert.That(m_pb.faceCount, Is.EqualTo(5));
-        }
-
-        [Test]
-        public void CreateShapeFromPolygon_NormalFacing()
-        {
-            m_Poly.m_Points.Add(new Vector3(0, 0, 0));
-            m_Poly.m_Points.Add(new Vector3(0, 0, 2));
-            m_Poly.m_Points.Add(new Vector3(2, 0, 0));
-            m_Poly.extrude = 0.0f;
-            m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
-            Vector3 nrm = Math.Normal(m_pb, m_pb.facesInternal[0]);
-            Assert.That(nrm, Is.EqualTo(Vector3.up));
-
-            m_pb.Clear();
-            //Changing the winding order should lead to a norml in the other direction
-            m_Poly.m_Points.Reverse();
-            m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals);
-            nrm = Math.Normal(m_pb, m_pb.facesInternal[0]);
-            Assert.That(nrm, Is.EqualTo(Vector3.up));
-        }
-
-        [Test]
-        public void CreateShapeFromPolygon_Holes_Success()
-        {
-            // Test that creating a valid shape with a hole gives a result of Success
-            m_Poly.m_Points.Add(new Vector3(0, 0, 0));
-            m_Poly.m_Points.Add(new Vector3(0, 0, 2));
-            m_Poly.m_Points.Add(new Vector3(2, 0, 2));
-            m_Poly.m_Points.Add(new Vector3(2, 0, 0));
-
-            Vector3[][] holes = new Vector3[2][];
-            holes[0] = new[]
-            {
-                new Vector3(0.1f, 0, 0.1f),
-                new Vector3(0.1f, 0, 1.9f),
-                new Vector3(0.9f, 0, 1.9f),
-                new Vector3(0.9f, 0, 0.1f)
-            };
-
-            holes[1] = new[]
-            {
-                new Vector3(1.1f, 0, 0.1f),
-                new Vector3(1.9f, 0, 0.1f),
-                new Vector3(1.9f, 0, 1.9f),
-            };
-
-            var result = m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals, holes);
-            Assert.That(result.status, Is.EqualTo(ActionResult.Status.Success));            
-        }
-
-        [Test]
-        public void CreateShapeFromPolygon_CreateMeshFailure_TooFewPointsInHole()
-        {
-            // Test that creating a shape with an invalid holes parameter does nothing and reports an error.
-            m_Poly.m_Points.Add(new Vector3(0, 0, 0));
-            m_Poly.m_Points.Add(new Vector3(0, 0, 2));
-            m_Poly.m_Points.Add(new Vector3(2, 0, 2));
-            m_Poly.m_Points.Add(new Vector3(2, 0, 0));
-
-            Vector3[][] holes = new Vector3[2][];
-            holes[0] = new[]
-            {
-                new Vector3(0.1f, 0, 0.1f),
-                new Vector3(0.1f, 0, 1.9f),
-                new Vector3(0.9f, 0, 1.9f),
-                new Vector3(0.9f, 0, 0.1f)
-            };
-
-            holes[1] = new[]
-            {
-                new Vector3(1.1f, 0, 0.1f),
-                new Vector3(1.9f, 0, 0.1f),
-            };
-
-            var result = m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals, holes);
-            Assert.That(result.status, Is.EqualTo(ActionResult.Status.NoChange));
-            Assert.That(result.notification, Is.EqualTo("Too Few Points in hole 1"));
-        }
+        var result = m_pb.CreateShapeFromPolygon(m_Poly.m_Points, m_Poly.extrude, m_Poly.flipNormals, holes);
+        Assert.That(result.status, Is.EqualTo(ActionResult.Status.NoChange));
+        Assert.That(result.notification, Is.EqualTo("Too Few Points in hole 1"));
     }
 }
--- Tests/Runtime/Type/EdgeTests.cs
+++ Tests/Runtime/Type/EdgeTests.cs
@@ -1,58 +1,56 @@
 using UnityEngine;
 using NUnit.Framework;
 using System;
+using UnityEngine.ProBuilder;
 
-namespace UnityEngine.ProBuilder.RuntimeTests.Type
+public static class EdgeTests
 {
-    public static class EdgeTests
+    public class EdgeComparisonTestCase
     {
-        public class EdgeComparisonTestCase
+        public Edge a, b;
+        public bool expected;
+
+        public EdgeComparisonTestCase(Edge a, Edge b, bool res)
         {
-            public Edge a, b;
-            public bool expected;
-
-            public EdgeComparisonTestCase(Edge a, Edge b, bool res)
-            {
-                this.a = a;
-                this.b = b;
-                expected = res;
-            }
-
-            public override string ToString()
-            {
-                return $"{a} == {b} = {expected}";
-            }
+            this.a = a;
+            this.b = b;
+            expected = res;
         }
 
-        static readonly EdgeComparisonTestCase[] s_EdgeEqualityNonSequential = new EdgeComparisonTestCase[]
+        public override string ToString()
         {
-            new EdgeComparisonTestCase(new Edge(0, 1), new Edge(0, 1), true),
-            new EdgeComparisonTestCase(new Edge(0, 1), new Edge(1, 0), true),
-            new EdgeComparisonTestCase(new Edge(0, 1), new Edge(1, 1), false),
-            new EdgeComparisonTestCase(new Edge(-1, 1), new Edge(-1, 1), true),
-            new EdgeComparisonTestCase(new Edge(-1, 1), new Edge(1, -1), true),
-            new EdgeComparisonTestCase(new Edge(0, 0), new Edge(0, 0), true),
-            new EdgeComparisonTestCase(new Edge(42901, 399928), Edge.Empty, false),
-            new EdgeComparisonTestCase(new Edge(42901, 399928), new Edge(42901, 399928), true),
-            new EdgeComparisonTestCase(new Edge(42901, 399928), new Edge(399928, 42901), true)
-        };
-
-        [Test]
-        public static void EdgeEquals_AllowsNonSequentialEquality([ValueSource("s_EdgeEqualityNonSequential")] EdgeComparisonTestCase test)
-        {
-            Assert.That(test.a.Equals(test.b), Is.EqualTo(test.expected));
+            return $"{a} == {b} = {expected}";
         }
+    }
 
-        [Test]
-        public static void EdgeEqualsOperator_AllowsNonSequentialEquality([ValueSource("s_EdgeEqualityNonSequential")] EdgeComparisonTestCase test)
-        {
-            Assert.That(test.a == test.b, Is.EqualTo(test.expected));
-        }
+    static readonly EdgeComparisonTestCase[] s_EdgeEqualityNonSequential = new EdgeComparisonTestCase[]
+    {
+        new EdgeComparisonTestCase(new Edge(0, 1), new Edge(0, 1), true),
+        new EdgeComparisonTestCase(new Edge(0, 1), new Edge(1, 0), true),
+        new EdgeComparisonTestCase(new Edge(0, 1), new Edge(1, 1), false),
+        new EdgeComparisonTestCase(new Edge(-1, 1), new Edge(-1, 1), true),
+        new EdgeComparisonTestCase(new Edge(-1, 1), new Edge(1, -1), true),
+        new EdgeComparisonTestCase(new Edge(0, 0), new Edge(0, 0), true),
+        new EdgeComparisonTestCase(new Edge(42901, 399928), Edge.Empty, false),
+        new EdgeComparisonTestCase(new Edge(42901, 399928), new Edge(42901, 399928), true),
+        new EdgeComparisonTestCase(new Edge(42901, 399928), new Edge(399928, 42901), true)
+    };
 
-        [Test]
-        public static void EdgeHashCode_IsSameForNonSequentialIndexes([ValueSource("s_EdgeEqualityNonSequential")] EdgeComparisonTestCase test)
-        {
-            Assert.That(test.a.GetHashCode() == test.b.GetHashCode(), Is.EqualTo(test.expected));
-        }
+    [Test]
+    public static void EdgeEquals_AllowsNonSequentialEquality([ValueSource("s_EdgeEqualityNonSequential")] EdgeComparisonTestCase test)
+    {
+        Assert.That(test.a.Equals(test.b), Is.EqualTo(test.expected));
+    }
+
+    [Test]
+    public static void EdgeEqualsOperator_AllowsNonSequentialEquality([ValueSource("s_EdgeEqualityNonSequential")] EdgeComparisonTestCase test)
+    {
+        Assert.That(test.a == test.b, Is.EqualTo(test.expected));
+    }
+
+    [Test]
+    public static void EdgeHashCode_IsSameForNonSequentialIndexes([ValueSource("s_EdgeEqualityNonSequential")] EdgeComparisonTestCase test)
+    {
+        Assert.That(test.a.GetHashCode() == test.b.GetHashCode(), Is.EqualTo(test.expected));
     }
 }
