603576b18842d5836a4261100f223ddaa098594f
Karl Henkel <karlh@unity3d.com>
2019-11-29 15:56:33 -0500

    move runtime tests out of deeply nested namespaces

--- Tests/Runtime/MeshOps/BridgeEdgesTests.cs
+++ Tests/Runtime/MeshOps/BridgeEdgesTests.cs
@@ -1,59 +1,57 @@
 using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
 using UnityEngine.ProBuilder.Tests.Framework;
 
-namespace UnityEngine.ProBuilder.RuntimeTests.MeshOperations
+static class BridgeEdgesTests
 {
-    static class BridgeEdgesTests
+    [Test]
+    public static void Bridge_TwoEdges_CreatesQuad()
     {
-        [Test]
-        public static void Bridge_TwoEdges_CreatesQuad()
-        {
-            var cube = ShapeGenerator.CreateShape(ShapeType.Cube);
-            cube.DeleteFace(cube.faces[0]);
-            var holes = ElementSelection.FindHoles(cube, cube.sharedVerticesInternal.Select(x => x[0]));
-            Assert.AreEqual(1, holes.Count, "found exactly 1 hole");
-            var bridgedFace = cube.Bridge(holes[0][0], holes[0][2]);
-            Assert.IsNotNull(bridgedFace);
-            Assert.AreEqual(4, bridgedFace.edgesInternal.Length);
-            cube.ToMesh();
-            cube.Refresh();
+        var cube = ShapeGenerator.CreateShape(ShapeType.Cube);
+        cube.DeleteFace(cube.faces[0]);
+        var holes = ElementSelection.FindHoles(cube, cube.sharedVertices.Select(x => x[0]));
+        Assert.AreEqual(1, holes.Count, "found exactly 1 hole");
+        var bridgedFace = cube.Bridge(holes[0][0], holes[0][2]);
+        Assert.IsNotNull(bridgedFace);
+        Assert.AreEqual(4, bridgedFace.edgesInternal.Length);
+        cube.ToMesh();
+        cube.Refresh();
 
 #if PB_CREATE_TEST_MESH_TEMPLATES
-            TestUtility.SaveAssetTemplate(cube.mesh, cube.name);
+        TestUtility.SaveAssetTemplate(cube.mesh, cube.name);
 #endif
-            TestUtility.AssertMeshAttributesValid(cube.mesh);
-            var template = TestUtility.GetAssetTemplate<Mesh>(cube.name);
-            Assert.IsNotNull(template);
-            TestUtility.AssertAreEqual(template, cube.mesh);
+        TestUtility.AssertMeshAttributesValid(cube.mesh);
+        var template = TestUtility.GetAssetTemplate<Mesh>(cube.name);
+        Assert.IsNotNull(template);
+        TestUtility.AssertAreEqual(template, cube.mesh);
 
-            UnityEngine.Object.DestroyImmediate(cube);
-        }
+        UnityEngine.Object.DestroyImmediate(cube);
+    }
 
-        [Test]
-        public static void Bridge_TwoConnectedEdges_CreatesTriangle()
-        {
-            var cube = ShapeGenerator.CreateShape(ShapeType.Cube);
-            cube.DeleteFace(cube.faces[0]);
-            var holes = ElementSelection.FindHoles(cube, cube.sharedVerticesInternal.Select(x => x[0]));
-            Assert.AreEqual(1, holes.Count, "found exactly 1 hole");
-            var bridgedFace = cube.Bridge(holes[0][0], holes[0][1]);
-            Assert.IsNotNull(bridgedFace);
-            Assert.AreEqual(3, bridgedFace.edgesInternal.Length);
-            cube.ToMesh();
-            cube.Refresh();
+    [Test]
+    public static void Bridge_TwoConnectedEdges_CreatesTriangle()
+    {
+        var cube = ShapeGenerator.CreateShape(ShapeType.Cube);
+        cube.DeleteFace(cube.faces[0]);
+        var holes = ElementSelection.FindHoles(cube, cube.sharedVertices.Select(x => x[0]));
+        Assert.AreEqual(1, holes.Count, "found exactly 1 hole");
+        var bridgedFace = cube.Bridge(holes[0][0], holes[0][1]);
+        Assert.IsNotNull(bridgedFace);
+        Assert.AreEqual(3, bridgedFace.edgesInternal.Length);
+        cube.ToMesh();
+        cube.Refresh();
 
 #if PB_CREATE_TEST_MESH_TEMPLATES
-            TestUtility.SaveAssetTemplate(cube.mesh, cube.name);
+        TestUtility.SaveAssetTemplate(cube.mesh, cube.name);
 #endif
-            TestUtility.AssertMeshAttributesValid(cube.mesh);
-            var template = TestUtility.GetAssetTemplate<Mesh>(cube.name);
-            Assert.IsNotNull(template);
-            TestUtility.AssertAreEqual(template, cube.mesh);
+        TestUtility.AssertMeshAttributesValid(cube.mesh);
+        var template = TestUtility.GetAssetTemplate<Mesh>(cube.name);
+        Assert.IsNotNull(template);
+        TestUtility.AssertAreEqual(template, cube.mesh);
 
-            UnityEngine.Object.DestroyImmediate(cube);
-        }
+        UnityEngine.Object.DestroyImmediate(cube);
     }
 }
--- Tests/Runtime/MeshOps/VertexColorTests.cs
+++ Tests/Runtime/MeshOps/VertexColorTests.cs
@@ -2,32 +2,30 @@ using System;
 using UnityEngine;
 using UObject = UnityEngine.Object;
 using NUnit.Framework;
+using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.Tests.Framework;
 
-namespace UnityEngine.ProBuilder.RuntimeTests.MeshOperations
+static class VertexColorTests
 {
-    static class VertexColorTests
+    [Test]
+    public static void DuplicateMesh_ApplyColor_MatchesTemplate()
     {
-        [Test]
-        public static void DuplicateMesh_ApplyColor_MatchesTemplate()
-        {
-            var cube = ShapeGenerator.CreateShape(ShapeType.Cube);
-            var dup = UObject.Instantiate(cube.gameObject).GetComponent<ProBuilderMesh>();
+        var cube = ShapeGenerator.CreateShape(ShapeType.Cube);
+        var dup = UObject.Instantiate(cube.gameObject).GetComponent<ProBuilderMesh>();
 
-            dup.SetFaceColor(dup.faces[0], Color.blue);
+        dup.SetFaceColor(dup.faces[0], Color.blue);
 
-            dup.ToMesh();
-            dup.Refresh();
+        dup.ToMesh();
+        dup.Refresh();
 
 #if PB_CREATE_TEST_MESH_TEMPLATES
-            TestUtility.SaveAssetTemplate(dup.mesh, dup.name);
+        TestUtility.SaveAssetTemplate(dup.mesh, dup.name);
 #endif
-            var compare = TestUtility.GetAssetTemplate<Mesh>(dup.name);
+        var compare = TestUtility.GetAssetTemplate<Mesh>(dup.name);
 
-            TestUtility.AssertAreEqual(compare, dup.mesh);
+        TestUtility.AssertAreEqual(compare, dup.mesh);
 
-            UObject.DestroyImmediate(cube);
-            UObject.DestroyImmediate(dup);
-        }
+        UObject.DestroyImmediate(cube);
+        UObject.DestroyImmediate(dup);
     }
 }
