b0ee65bd855de78ea991b62da0cfc0726fb9a4e2
Roxanne Corbeil <roxanne.corbeil@unity3d.com>
2020-05-13 14:22:02 -0400

    Optimization

--- Editor/MenuActions/Selection/SelectPathFaces.cs
+++ Editor/MenuActions/Selection/SelectPathFaces.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
 using System.Linq;
+using Unity.Profiling;
+using System;
 
 namespace UnityEditor.ProBuilder.Actions
 {
     public static class SelectPathFaces
     {
         // cache the nodes ?
+        // cache the weight array
 
         public static List<int> GetPath(int start, int end, ProBuilderMesh mesh)
         {
@@ -19,124 +22,143 @@ namespace UnityEditor.ProBuilder.Actions
 
         private static int[] Dijkstra(int start, int end, ProBuilderMesh mesh)
         {
-            List<int> visited = new List<int>();
-            List<int> unvisited = new List<int>();
-            int faceCount = mesh.faceCount;
-         
-            double[] weights = new double[faceCount];
-            int[] predecessors = new int[faceCount];
-            List<Node> nodes = GetNodes(start, mesh);
-
-            for (int i = 0; i < faceCount; i++)
+            Debug.Log("??");
+            using (new ProfilerMarker("Select path Dijkstra").Auto())
             {
-                weights[i] = double.MaxValue;
-                unvisited.Add(i);
-            }
+                List<int> visited = new List<int>();
+                List<int> unvisited = new List<int>();
+                int faceCount = mesh.faceCount;
 
-            int current = start;
-            weights[current] = 0;
-            visited.Add(current);
-            unvisited.Remove(current);
+                double[] weights = new double[faceCount];
+                int[] predecessors = new int[faceCount];
+                List<Node> nodes = GetNodes(start, mesh);
 
-            do
-            {
-                for (int other = 0; other < unvisited.Count; other++)
+                for (int i = 0; i < faceCount; i++)
                 {
-                    var node = nodes.Where(x => x.Start == current && x.End == unvisited[other]).FirstOrDefault();
-                    if (node.Weight == 0)
-                        continue;
-                    if (weights[current] + node.Weight < weights[unvisited[other]])
-                    {
-                        var idx = unvisited[other];
-                        var idxU = current;
-                        weights[unvisited[other]] = weights[current] + node.Weight;
-                        predecessors[unvisited[other]] = current;
-                    }
-                    else
-                    {
-                        var idx = unvisited[other];
-                        var idxU = current;
-                    }
+                    weights[i] = double.MaxValue;
+                    unvisited.Add(i);
                 }
 
-                double min = double.MaxValue;
-
-                for (int i = 0; i < visited.Count; i++)
-                {
-                    for (int j = 0; j < unvisited.Count; j++)
-                    {
-                        var node = nodes.Where(x => x.Start == visited[i] && x.End == unvisited[j]).FirstOrDefault();
-                        if (node.Weight == 0)
-                            continue;
-                        if (node.Weight < min)
-                        {
-                            min = node.Weight;
-                            current = unvisited[j];
-                        }
-                    }
-                }
-
+                int current = start;
+                weights[current] = 0;
                 visited.Add(current);
                 unvisited.Remove(current);
 
-            } while (visited.Count < faceCount && !visited.Contains(end));
+       
 
-            return predecessors;
+                do
+                {
+                    using (new ProfilerMarker("Select path Dijkstra first loop").Auto())
+                    {
+                        for (int other = 0; other < unvisited.Count; other++)
+                        {
+                            //var node = nodes.Where(x => x.Start == current && x.End == unvisited[other]).FirstOrDefault();
+                            Node node = new Node();
+
+                            foreach (var nodei in nodes)
+                            {
+                                if (nodei.Start == current && nodei.End == unvisited[other])
+                                    node = nodei;
+                            }
+
+                            if (node.Weight == 0)
+                                continue;
+                            if (weights[current] + node.Weight < weights[unvisited[other]])
+                            {
+                                weights[unvisited[other]] = weights[current] + node.Weight;
+                                predecessors[unvisited[other]] = current;
+                            }
+                        }
+                    }
+
+                    double min = double.MaxValue;
+                    using (new ProfilerMarker("Select path Dijkstra second loop").Auto())
+                    {
+                        for (int i = 0; i < unvisited.Count; i++)
+                        {
+                            if (weights[unvisited[i]] < min)
+                            {
+                                min = weights[unvisited[i]];
+                                current = unvisited[i];
+                            }
+                        }
+                    }
+
+                    visited.Add(current);
+                    unvisited.Remove(current);
+
+                } while (visited.Count < faceCount && !visited.Contains(end));
+
+                return predecessors;
+            }
         }
 
         private static List<Node> GetNodes(int start, ProBuilderMesh mesh)
         {
-            List<Node> list = new List<Node>();
-            foreach (var face in mesh.faces)
+            using (new ProfilerMarker("Select path GetNodes").Auto())
             {
-                foreach (var other in mesh.faces)
+                List<Node> list = new List<Node>(mesh.facesInternal.Length * 4);
+                List<SimpleTuple<Face, Edge>> neighbors = new List<SimpleTuple<Face, Edge>>(8);
+                foreach (var face in mesh.facesInternal)
                 {
-                    if (face == other)
-                        continue;
-                    foreach (var edge in face.edges)
+                    foreach (var edge in face.edgesInternal)
                     {
-                        var neighbors = ElementSelection.GetNeighborFaces(mesh, edge);
+                        neighbors = ElementSelection.GetNeighborFaces(mesh, edge);
                         foreach (var neighbor in neighbors)
                         {
-                            if (neighbor.item1 != other)
+                            if (neighbor.item1 == face)
                                 continue;
-                            Node node = new Node(mesh.faces.IndexOf(face), mesh.faces.IndexOf(neighbor.item1), GetWeight(face, neighbor.item1));
+                            Node node = new Node(Array.IndexOf<Face>(mesh.facesInternal, face), Array.IndexOf<Face>(mesh.facesInternal, neighbor.item1), GetWeight(face, neighbor.item1, mesh));
                             list.Add(node);
                         }
                     }
-                }
-            }
 
-            return list;
+                }
+
+                return list;
+            }
         }
 
-        private static double GetWeight(Face face1, Face face2)
+        private static double GetWeight(Face face1, Face face2, ProBuilderMesh mesh)
         {
             double baseCost = 10.0;
             double normalCost = 2.0;
             double distCost = 3.0;
+
+           // var n1 = UnityEngine.ProBuilder.Math.Normal(mesh, face1);
+          //  var n2 = UnityEngine.ProBuilder.Math.Normal(mesh, face2);
+
+
+            //foreach ()
+            //{
+
+            //}
+
+            //var p1 = Math.Average(mesh.positionsInternal[face1.indexesInternal)
 
             return 1.0;
         }
 
         private static List<int> GetMinimalPath(int[] predecessors, int start, int end)
         {
-            List<int> list = new List<int>();
-            list.Add(end);
-            int a = end;
-            while (a != start)
+            using (new ProfilerMarker("Select path MinimalPath").Auto())
             {
-                a = predecessors[a];
-                list.Add(a);
+                List<int> list = new List<int>();
+                list.Add(end);
+                int a = end;
+                while (a != start)
+                {
+                    a = predecessors[a];
+                    list.Add(a);
+                }
+                for (int i = 0, j = list.Count - 1; i < j; i++)
+                {
+                    var item = list[j];
+                    list.Remove(item);
+                    list.Insert(i, item);
+                }
+                return list;
             }
-            for (int i = 0, j = list.Count - 1; i < j; i++)
-            {
-                var item = list[j];
-                list.Remove(item);
-                list.Insert(i, item);
-            }
-            return list;
-
         }
 
         private struct Node
