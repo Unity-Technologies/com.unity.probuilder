c481f6431c0714c81e5e8cb026dff9b0b28848d0
Thomas <thomas.lopez@unity3d.com>
2020-07-16 15:33:20 +0200

    Moved cut tool to editor tool and add custom cursor when cutting

--- Editor/MenuActions/Geometry/CutToolAction.cs
+++ Editor/MenuActions/Geometry/CutToolAction.cs
@@ -68,68 +68,72 @@ namespace UnityEditor.ProBuilder.Actions
         }
 
 
-        PolygonalCut m_CutTarget;
+        //PolygonalCut m_CutTarget;
 
         /// <summary>
         /// Called when the settings window is closed.
         /// </summary>
-        protected override void OnSettingsDisable()
-        {
-            if (m_CutTarget != null)
-                Undo.DestroyObjectImmediate(m_CutTarget);
-        }
-
-        protected override void OnSettingsGUI()
-        {
-            GUILayout.Label("Point-to-point Cut - Settings", EditorStyles.boldLabel);
-
-            EditorGUILayout.HelpBox("Add new vertices in the selected face. Press ESC to validate the shape. " +
-                                    "\nUsing CTRL key allows to snap the selection to existing edges and vertices. " +
-                                    "\nUsing SHIFT key allows to move points of the cut shape.", MessageType.Info);
-
-            EditorGUI.BeginChangeCheck();
-
-            s_ConnectToStart.value = EditorGUILayout.Toggle("Connect End to Start Point", s_ConnectToStart);
-
-            s_EndOnClicToStart.value = EditorGUILayout.Toggle("Selecting Start Point is ending cut", s_EndOnClicToStart);
-
-            s_EdgeToEdge.value = EditorGUILayout.Toggle("Cut From Edge To Edge", s_EdgeToEdge);
-
-            if (EditorGUI.EndChangeCheck())
-                ProBuilderSettings.Save();
-
-            GUILayout.FlexibleSpace();
-
-            if (m_CutTarget == null)
-            {
-                if (GUILayout.Button("Start Vertices Insertion"))
-                    DoAction();
-            }
-            else
-            {
-                if (GUILayout.Button("Compute Cut"))
-                    DoCut();
-            }
-        }
+        // protected override void OnSettingsDisable()
+        // {
+        //     if (m_CutTarget != null)
+        //         Undo.DestroyObjectImmediate(m_CutTarget);
+        // }
+        //
+        // protected override void OnSettingsGUI()
+        // {
+        //     GUILayout.Label("Point-to-point Cut - Settings", EditorStyles.boldLabel);
+        //
+        //     EditorGUILayout.HelpBox("Add new vertices in the selected face. Press ESC to validate the shape. " +
+        //                             "\nUsing CTRL key allows to snap the selection to existing edges and vertices. " +
+        //                             "\nUsing SHIFT key allows to move points of the cut shape.", MessageType.Info);
+        //
+        //     EditorGUI.BeginChangeCheck();
+        //
+        //     s_ConnectToStart.value = EditorGUILayout.Toggle("Connect End to Start Point", s_ConnectToStart);
+        //
+        //     s_EndOnClicToStart.value = EditorGUILayout.Toggle("Selecting Start Point is ending cut", s_EndOnClicToStart);
+        //
+        //     s_EdgeToEdge.value = EditorGUILayout.Toggle("Cut From Edge To Edge", s_EdgeToEdge);
+        //
+        //     if (EditorGUI.EndChangeCheck())
+        //         ProBuilderSettings.Save();
+        //
+        //     GUILayout.FlexibleSpace();
+        //
+        //     if (m_CutTarget == null)
+        //     {
+        //         if (GUILayout.Button("Start Vertices Insertion"))
+        //             DoAction();
+        //     }
+        //     else
+        //     {
+        //         if (GUILayout.Button("Compute Cut"))
+        //             DoCut();
+        //     }
+        // }
 
         public override ActionResult DoAction()
         {
-            if (MeshSelection.selectedObjectCount < 1)
-                return ActionResult.NoSelection;
+            // if (MeshSelection.selectedObjectCount < 1)
+            //     return ActionResult.NoSelection;
+            //
+            // if (MeshSelection.selectedObjectCount > 1)
+            //     return new ActionResult(ActionResult.Status.Failure, "Only one ProBuilder object must be selected");
+            //
+            // ProBuilderMesh firstObj = MeshSelection.activeMesh;
+            //
+            // m_CutTarget = Undo.AddComponent<PolygonalCut>(firstObj.gameObject);
+            //
+            // return new ActionResult(ActionResult.Status.Success,"Vertex On Face Insertion");
 
-            if (MeshSelection.selectedObjectCount > 1)
-                return new ActionResult(ActionResult.Status.Failure, "Only one ProBuilder object must be selected");
-
-            ProBuilderMesh firstObj = MeshSelection.activeMesh;
-
-            m_CutTarget = Undo.AddComponent<PolygonalCut>(firstObj.gameObject);
-
-            return new ActionResult(ActionResult.Status.Success,"Vertex On Face Insertion");
+            ProBuilderEditor.selectMode = SelectMode.Object;
+            EditorTools.EditorTools.SetActiveTool<CutTool>();
+            return new ActionResult(ActionResult.Status.Success,"Cut Tool Started");
         }
 
-        private void DoCut()
-        {
-            m_CutTarget.doCut = true;
-        }
+        // private void DoCut()
+        // {
+        //     m_CutTarget.doCut = true;
+        // }
     }
 }
