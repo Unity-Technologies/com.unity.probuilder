3e8391be48a1eb752a8aac8aee92727a7136bbe3
Neil Devine <neil@unity3d.com>
2020-08-31 11:05:07 -0400

    wip

--- Editor/EditorCore/ProBuilderMeshEditor.cs
+++ Editor/EditorCore/ProBuilderMeshEditor.cs
@@ -82,6 +82,16 @@ namespace UnityEditor.ProBuilder
         {
             VertexManipulationTool.beforeMeshModification -= OnBeginMeshModification;
             VertexManipulationTool.afterMeshModification -= OnFinishMeshModification;
+
+//            if (m_PreviewUtility != null)
+//            {
+//                m_PreviewUtility.Cleanup();
+//                m_PreviewUtility = null;
+//            }
+//            if (m_WireMaterial)
+//            {
+//                DestroyImmediate(m_WireMaterial, true);
+//            }
         }
 
         void OnBeginMeshModification(IEnumerable<ProBuilderMesh> selection)
@@ -121,6 +131,12 @@ namespace UnityEditor.ProBuilder
 
             if (m_Mesh.TryGetComponent(out filter) && (sharedMesh = filter.sharedMesh) != null)
             {
+                // When inspecting a prefab asset the AssetDatabase continually loops on some very expensive operations
+                serializedObject.Update();
+                EditorGUI.BeginChangeCheck();
+                LightmapStaticSettings();
+                if (EditorGUI.EndChangeCheck())
+                    serializedObject.ApplyModifiedProperties();
                 GUILayout.Label("Vertex Count: " + sharedMesh.vertexCount);
                 GUILayout.Label("Submesh Count: " + sharedMesh.subMeshCount);
             }
@@ -302,11 +318,229 @@ namespace UnityEditor.ProBuilder
 
             return new Bounds((min + max) / 2f, max != min ? max - min : Vector3.one * .1f);
         }
+//
+//        private PreviewRenderUtility m_PreviewUtility;
+//        private Material m_Material;
+//        private Material m_WireMaterial;
+//        public Vector2 previewDir = new Vector2(-120, 20);
+//
+//        void Init()
+//        {
+//            if (m_PreviewUtility == null)
+//            {
+//                m_PreviewUtility = new PreviewRenderUtility();
+//                m_PreviewUtility.camera.fieldOfView = 30.0f;
+//                m_Material = BuiltinMaterials.defaultMaterial;
+//                m_WireMaterial = ModelInspector.CreateWireframeMaterial();
+//            }
+//        }
+//
+//        public override void OnPreviewSettings()
+//        {
+//            if (!ShaderUtil.hardwareSupportsRectRenderTexture)
+//                return;
+//            GUI.enabled = true;
+//            Init();
+//        }
+//
+//        private void DoRenderPreview()
+//        {
+//            var t = target as ProBuilderMesh;
+//            Mesh m;
+//            if (t.mesh != null)
+//                m  = t.mesh;
+//            else
+//            {
+//                m = new Mesh();
+//                UnityEngine.ProBuilder.MeshUtility.Compile(t,m);
+//            }
+//            ModelInspector.RenderMeshPreview(m, m_PreviewUtility, m_Material, m_WireMaterial, previewDir, -1);
+//        }
+//
+//        public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
+//        {
+//            if (!ShaderUtil.hardwareSupportsRectRenderTexture)
+//            {
+//                Debug.Log("Could not generate static preview. Render texture not supported by hardware.");
+//                return null;
+//            }
+//
+//            Init();
+//
+//            m_PreviewUtility.BeginStaticPreview(new Rect(0, 0, width, height));
+//
+//            DoRenderPreview();
+//
+//            return m_PreviewUtility.EndStaticPreview();
+//        }
+//
+//        public override bool HasPreviewGUI()
+//        {
+//            return (target != null);
+//        }
+//
+//        public override void OnPreviewGUI(Rect r, GUIStyle background)
+//        {
+//            if (!ShaderUtil.hardwareSupportsRectRenderTexture)
+//            {
+//                if (Event.current.type == EventType.Repaint)
+//                    EditorGUI.DropShadowLabel(new Rect(r.x, r.y, r.width, 40), "Mesh preview requires\nrender texture support");
+//                return;
+//            }
+//
+//            Init();
+//
+//            previewDir = PreviewGUI.Drag2D(previewDir, r);
+//
+//            if (Event.current.type != EventType.Repaint)
+//                return;
+//
+//            m_PreviewUtility.BeginPreview(r, background);
+//
+//            DoRenderPreview();
+//
+//            m_PreviewUtility.EndAndDrawPreview(r);
+//        }
+//
+//        // A minimal list of settings to be shown in the Asset Store preview inspector
+//        internal override void OnAssetStoreInspectorGUI()
+//        {
+//            OnInspectorGUI();
+//        }
+//
+//        public override string GetInfoString()
+//        {
+//            var t = target as ProBuilderMesh;
+//            t.Rebuild();
+//            Mesh mesh = t.mesh;
+//            string info = mesh.vertexCount + " verts, " + InternalMeshUtil.GetPrimitiveCount(mesh) + " tris";
+//            int submeshes = mesh.subMeshCount;
+//            if (submeshes > 1)
+//                info += ", " + submeshes + " submeshes";
+//
+//            int blendShapeCount = mesh.blendShapeCount;
+//            if (blendShapeCount > 0)
+//                info += ", " + blendShapeCount + " blendShapes";
+//
+//            info += "\n" + InternalMeshUtil.GetVertexFormat(mesh);
+//            return info;
+//        }
 
         [MenuItem("CONTEXT/ProBuilderMesh/Open ProBuilder")]
         static void OpenProBuilder()
         {
             ProBuilderEditor.MenuOpenWindow();
+        }
+    }
+
+    //[CustomPreview(typeof(ProBuilderMesh))]
+    public class ProBuilderMeshPreview : ObjectPreview
+    {
+        static PreviewRenderUtility s_PreviewUtility;
+        static Material s_Material;
+        static Material s_WireMaterial;
+        static Vector2 s_PreviewDir = new Vector2(-120, 20);
+
+        static readonly Dictionary<Object, Mesh> s_Meshes = new Dictionary<Object, Mesh>();
+
+
+        [InitializeOnLoadMethod]
+        static void InitOnLoad()
+        {
+            ActiveEditorTracker.editorTrackerRebuilt += ActiveEditorTrackerOnEditorTrackerRebuilt;
+        }
+
+        static void ActiveEditorTrackerOnEditorTrackerRebuilt()
+        {
+            s_Meshes.Clear();
+            foreach (var allInspectorWindow in InspectorWindow.GetAllInspectorWindows())
+            {
+                foreach (var activeEditor in allInspectorWindow.tracker.activeEditors)
+                {
+                    if (activeEditor is ProBuilderMeshEditor)
+                    {
+                         foreach (var o in activeEditor.targets)
+                         {
+                             var t = o as ProBuilderMesh;
+                             if (!s_Meshes.ContainsKey(t))
+                             {
+                                 if (t.mesh != null)
+                                     s_Meshes[t] = t.mesh;
+                                 else
+                                 {
+                                     var m = new Mesh();
+                                     UnityEngine.ProBuilder.MeshUtility.Compile(t,m);
+                                     s_Meshes[t] = m;
+                                 }
+                             }
+                         }
+                    }
+                }
+            }
+        }
+
+        static void Init()
+        {
+            if (s_PreviewUtility != null) return;
+
+            s_PreviewUtility = new PreviewRenderUtility();
+            s_PreviewUtility.camera.fieldOfView = 30.0f;
+            s_Material = BuiltinMaterials.defaultMaterial;
+            s_WireMaterial = ModelInspector.CreateWireframeMaterial();
+        }
+
+        public override void OnPreviewSettings()
+        {
+            if (!ShaderUtil.hardwareSupportsRectRenderTexture)
+                return;
+            GUI.enabled = true;
+            Init();
+        }
+
+
+        public override bool HasPreviewGUI()
+        {
+            return (target != null);
+        }
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background)
+        {
+            if (!ShaderUtil.hardwareSupportsRectRenderTexture)
+            {
+                if (Event.current.type == EventType.Repaint)
+                    EditorGUI.DropShadowLabel(new Rect(r.x, r.y, r.width, 40), "Mesh preview requires\nrender texture support");
+                return;
+            }
+
+            Init();
+
+            s_PreviewDir = PreviewGUI.Drag2D(s_PreviewDir, r);
+
+            if (Event.current.type != EventType.Repaint)
+                return;
+
+            s_PreviewUtility.BeginPreview(r, background);
+
+            var t = target as ProBuilderMesh;
+            ModelInspector.RenderMeshPreview(s_Meshes[t], s_PreviewUtility, s_Material, s_WireMaterial, s_PreviewDir, -1);
+
+            s_PreviewUtility.EndAndDrawPreview(r);
+        }
+
+        public override string GetInfoString()
+        {
+            var mesh = s_Meshes[target];
+            var info = $"{mesh.vertexCount} verts, {InternalMeshUtil.GetPrimitiveCount(mesh)} tris";
+            var subMeshes = mesh.subMeshCount;
+            if (subMeshes > 1)
+                info += $", {subMeshes} submeshes";
+
+            var blendShapeCount = mesh.blendShapeCount;
+            if (blendShapeCount > 0)
+                info += $", {blendShapeCount} blendShapes";
+
+            info += $"\n{InternalMeshUtil.GetVertexFormat(mesh)}";
+            return info;
         }
     }
 }
