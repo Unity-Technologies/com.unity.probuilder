d2a7b2929c67bea40953b322f25d71e69c5021d4
Thomas <thomas.lopez@unity3d.com>
2021-01-22 15:21:35 +0100

    fix for light theme icons

--- Editor/EditorCore/IconUtility.cs
+++ Editor/EditorCore/IconUtility.cs
@@ -37,7 +37,7 @@ namespace UnityEditor.ProBuilder
 
             bool isDarkSkin = skin == IconSkin.Default ? EditorGUIUtility.isProSkin : skin == IconSkin.Pro;
             string name = isDarkSkin ? iconName : iconName + "_Light";
-            name = EditorGUIUtility.pixelsPerPoint>1 ? iconName + "@2x" : iconName;
+            name = EditorGUIUtility.pixelsPerPoint>1 ? iconName+"@2x" : name;
 
             Texture2D icon = null;
 
