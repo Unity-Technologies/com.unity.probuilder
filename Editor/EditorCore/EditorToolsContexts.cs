35bfb9c8baf8dd42430029f0b06fe1479c64c419
Mod Rimkus <mod.rimkus@unity3d.com>
2021-09-28 15:08:47 -0400

    Added barebones selection mode toggles to ProBuilderToolContext's settings

--- Editor/EditorCore/EditorToolsContexts.cs
+++ Editor/EditorCore/EditorToolsContexts.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor.EditorTools;
+using UnityEditor.Overlays;
+using UnityEditor.Toolbars;
 using UnityEngine;
 
 namespace UnityEditor.ProBuilder
 {
 #if UNITY_2020_2_OR_NEWER
+
     [EditorToolContext("ProBuilder"), Icon(k_IconPath)]
     class ProBuilderToolContext : EditorToolContext
     {
@@ -28,6 +32,39 @@ namespace UnityEditor.ProBuilder
         }
     }
 
+    [CustomEditor(typeof(ProBuilderToolContext))]
+    class ProBuilderToolContextEditor : Editor, ICreateToolbar
+    {
+        public IEnumerable<string> toolbarElements
+        {
+            get
+            {
+                yield return "Selection Settings Toolbar";
+            }
+        }
+    }
+
+    [EditorToolbarElement("Selection Settings Toolbar", typeof(SceneView))]
+    class SelectionSettingsToolbar : OverlayToolbar
+    {
+        SelectionSettingsToolbar()
+        {
+            var object_Graphic_off = IconUtility.GetIcon("Modes/Mode_Object");
+            var face_Graphic_off = IconUtility.GetIcon("Modes/Mode_Face");
+            var vertex_Graphic_off = IconUtility.GetIcon("Modes/Mode_Vertex");
+            var edge_Graphic_off = IconUtility.GetIcon("Modes/Mode_Edge");
+
+            name = "ProBuilder Selection Settings";
+
+            Add(new EditorToolbarToggle("Object Selection", object_Graphic_off, object_Graphic_off));
+            Add(new EditorToolbarToggle("Vertex Selection", vertex_Graphic_off, vertex_Graphic_off));
+            Add(new EditorToolbarToggle("Edge Selection", edge_Graphic_off, edge_Graphic_off));
+            Add(new EditorToolbarToggle("Face Selection", face_Graphic_off, face_Graphic_off));
+
+            SetupChildrenAsButtonStrip();
+        }
+    }
+
     class TextureToolContext : EditorToolContext
     {
         TextureToolContext() { }
@@ -47,5 +84,6 @@ namespace UnityEditor.ProBuilder
             }
         }
     }
+
 #endif
 }
