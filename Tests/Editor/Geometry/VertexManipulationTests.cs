3765faa26dc035b4edd2192894b14e9ef3230eba
Mod Rimkus <mod.rimkus@unity3d.com>
2021-11-26 22:48:54 -0500

    Test fix

--- Tests/Editor/Geometry/VertexManipulationTests.cs
+++ Tests/Editor/Geometry/VertexManipulationTests.cs
@@ -6,6 +6,7 @@ using UnityEditor.ProBuilder;
 using UnityEngine.ProBuilder;
 using UnityEngine.TestTools;
 using NUnit.Framework;
+using UnityEditor.EditorTools;
 using UObject = UnityEngine.Object;
 
 class VertexManipulationTests
@@ -13,10 +14,28 @@ class VertexManipulationTests
     [Test]
     public static void ExtrudeOrthogonally_OneElementManyTimes_NoYOffsetAccumulates()
     {
-        // Generate 1 face plane
+        // Generate single face plane
         var pb = ShapeGenerator.GeneratePlane(PivotLocation.Center, 1f, 1f, 0, 0, Axis.Up);
         try
         {
+            pb.transform.position = Vector3.zero;
+            pb.transform.rotation = Quaternion.identity;
+
+            ProBuilderEditor.MenuOpenWindow();
+            EditorApplication.ExecuteMenuItem("Window/General/Scene");
+
+            var sceneView = UnityEngine.Resources.FindObjectsOfTypeAll<UnityEditor.SceneView>()[0];
+            sceneView.orthographic = true;
+            sceneView.drawGizmos = false;
+            sceneView.pivot = new Vector3(0, 0, 0);
+            sceneView.rotation = Quaternion.AngleAxis(90f, Vector3.right);
+            sceneView.size = 2.0f;
+            sceneView.Focus();
+
+            var e = new Event();
+            e.type = EventType.MouseEnterWindow;
+            sceneView.SendEvent(e);
+
             Assume.That(pb.facesInternal.Length, Is.EqualTo(1));
             var face = pb.facesInternal[0];
 
@@ -24,38 +43,42 @@ class VertexManipulationTests
             var selectedFaces = new List<Face>();
             selectedFaces.Add(face);
             ProBuilderEditor.toolManager.SetSelectMode(SelectMode.Face);
+            ToolManager.SetActiveTool(typeof(ProbuilderMoveTool));
             pb.SetSelectedFaces(selectedFaces);
             MeshSelection.SetSelection(pb.gameObject);
 
-            // Look directly at the plane and focus scene view
-            var sceneView = SceneView.lastActiveSceneView;
-            sceneView.LookAtDirect(pb.transform.position, Quaternion.LookRotation(Vector3.down), 1f);
-            sceneView.Focus();
-
-            var mousePos = sceneView.position.size * .5f;
-            mousePos.y += sceneView.rootVisualElement.worldBound.y;
+            // Center mouse position
+            var bounds = SceneView.focusedWindow.rootVisualElement.worldBound;
+            var mousePos = (bounds.size * 0.5f + bounds.position) + new Vector2Int(1, 1);
 
             const int k_ExtrudeCount = 100;
             for (int i = 0; i < k_ExtrudeCount; i++)
             {
                 // Press down at the center of the face
-                UnityEngine.Event e = new UnityEngine.Event()
+                e = new UnityEngine.Event()
                 {
                     type = EventType.MouseDown,
                     mousePosition = mousePos,
-                    modifiers = EventModifiers.Shift,
+                    modifiers = EventModifiers.None,
                     clickCount = 1,
                     delta = Vector2.zero,
                 };
                 sceneView.SendEvent(e);
 
                 // Do lateral 1px drag and release
+                var mouseDelta =  new Vector2(i % 2 == 0 ? 1f : -1f, 0f);
+                mousePos += mouseDelta;
+
                 e.type = EventType.MouseDrag;
-                e.delta = new Vector2(1f, 0f);
-                mousePos += e.delta;
                 e.mousePosition = mousePos;
+                e.modifiers = EventModifiers.Shift;
+                e.clickCount = 0;
+                e.delta = mouseDelta;
                 sceneView.SendEvent(e);
+
                 e.type = EventType.MouseUp;
+                e.mousePosition = mousePos;
+                e.delta = Vector2.zero;
                 sceneView.SendEvent(e);
             }
 
@@ -70,7 +93,7 @@ class VertexManipulationTests
 
             // After many orthogonal extrusions, the last face should still be at y=0 coordinate
             for (int i = 0; i < faceVertices.Length; i++)
-                Assert.That(faceVertices[i].position.y, Is.EqualTo(0));
+                Assert.That(faceVertices[i].position.y, Is.EqualTo(0f));
         }
         finally
         {
