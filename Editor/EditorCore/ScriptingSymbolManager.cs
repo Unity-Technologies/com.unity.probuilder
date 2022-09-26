7a03cda5bd244a3efeb7ec9adf75d71e5c18a0d8
Thomas Lopez <thomas.lopez@unity3d.com>
2022-09-26 16:56:54 -0400

    Fixed a bug where `Experimental Features Enabled` was not activating when using `Dedicated Server` platform.

--- Editor/EditorCore/ScriptingSymbolManager.cs
+++ Editor/EditorCore/ScriptingSymbolManager.cs
@@ -1,17 +1,48 @@
 using System;
+#if UNITY_2021_2_OR_NEWER
+using UnityEditor.Build;
+#endif
 
 namespace UnityEditor.ProBuilder
 {
     static class ScriptingSymbolManager
     {
+#if UNITY_2021_2_OR_NEWER
+        static BuildPlatform[] k_ValidPlatforms = null;
+        static BuildPlatform[] ValidPlatforms
+        {
+            get
+            {
+                if (k_ValidPlatforms == null)
+                    k_ValidPlatforms = BuildPlatforms.instance.GetValidPlatforms(true).ToArray();
+
+                return k_ValidPlatforms;
+            }
+        }
+#else
         static bool IsObsolete(BuildTargetGroup group)
         {
             var attrs = typeof(BuildTargetGroup).GetField(group.ToString()).GetCustomAttributes(typeof(ObsoleteAttribute), false);
             return attrs.Length > 0;
         }
+#endif
 
         internal static bool ContainsDefine(string define)
         {
+#if UNITY_2021_2_OR_NEWER
+            foreach (BuildPlatform targetPlatform in ValidPlatforms)
+            {
+                if (targetPlatform.namedBuildTarget == NamedBuildTarget.Unknown)
+                    continue;
+
+                string defineSymbols = PlayerSettings.GetScriptingDefineSymbols(targetPlatform.namedBuildTarget);
+
+                if (!defineSymbols.Contains(define))
+                    return false;
+            }
+
+            return true;
+#else
             foreach (BuildTargetGroup targetGroup in System.Enum.GetValues(typeof(BuildTargetGroup)))
             {
                 if (targetGroup == BuildTargetGroup.Unknown || IsObsolete(targetGroup))
@@ -24,6 +55,7 @@ namespace UnityEditor.ProBuilder
             }
 
             return true;
+#endif
         }
 
         /// <summary>
@@ -32,6 +64,27 @@ namespace UnityEditor.ProBuilder
         /// <param name="define"></param>
         public static void AddScriptingDefine(string define)
         {
+#if UNITY_2021_2_OR_NEWER
+            foreach (BuildPlatform targetPlatform in ValidPlatforms)
+            {
+                if (targetPlatform.namedBuildTarget == NamedBuildTarget.Unknown)
+                    continue;
+
+                string defineSymbols = PlayerSettings.GetScriptingDefineSymbols(targetPlatform.namedBuildTarget);
+
+                if (!defineSymbols.Contains(define))
+                {
+                    if (defineSymbols.Length < 1)
+                        defineSymbols = define;
+                    else if (defineSymbols.EndsWith(";"))
+                        defineSymbols = string.Format("{0}{1}", defineSymbols, define);
+                    else
+                        defineSymbols = string.Format("{0};{1}", defineSymbols, define);
+
+                    PlayerSettings.SetScriptingDefineSymbols(targetPlatform.namedBuildTarget, defineSymbols);
+                }
+            }
+#else
             foreach (BuildTargetGroup targetGroup in System.Enum.GetValues(typeof(BuildTargetGroup)))
             {
                 if (targetGroup == BuildTargetGroup.Unknown || IsObsolete(targetGroup))
@@ -51,6 +104,7 @@ namespace UnityEditor.ProBuilder
                     PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, defineSymbols);
                 }
             }
+#endif
         }
 
         /// <summary>
@@ -59,6 +113,23 @@ namespace UnityEditor.ProBuilder
         /// <param name="define"></param>
         public static void RemoveScriptingDefine(string define)
         {
+#if UNITY_2021_2_OR_NEWER
+            foreach (BuildPlatform targetPlatform in ValidPlatforms)
+            {
+                if (targetPlatform.namedBuildTarget == NamedBuildTarget.Unknown)
+                    continue;
+
+                string defineSymbols = PlayerSettings.GetScriptingDefineSymbols(targetPlatform.namedBuildTarget);
+
+                if (defineSymbols.Contains(define))
+                {
+                    defineSymbols = defineSymbols.Replace(string.Format("{0};", define), "");
+                    defineSymbols = defineSymbols.Replace(define, "");
+
+                    PlayerSettings.SetScriptingDefineSymbols(targetPlatform.namedBuildTarget, defineSymbols);
+                }
+            }
+#else
             foreach (BuildTargetGroup targetGroup in System.Enum.GetValues(typeof(BuildTargetGroup)))
             {
                 if (targetGroup == BuildTargetGroup.Unknown || IsObsolete(targetGroup))
@@ -74,6 +145,7 @@ namespace UnityEditor.ProBuilder
                     PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, defineSymbols);
                 }
             }
+#endif
         }
     }
 }
