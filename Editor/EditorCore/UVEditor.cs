e430c5078768588ba33640b0f12fd3c7721dc9db
Karl Henkel <karlh@unity3d.com>
2023-04-05 10:59:19 -0400

    fix compile error on macos 2019.4

--- Editor/EditorCore/UVEditor.cs
+++ Editor/EditorCore/UVEditor.cs
@@ -3271,7 +3271,7 @@ namespace UnityEditor.ProBuilder
         Color screenshot_backgroundColor = Color.black;
         string screenShotPath = "";
 
-        readonly Color32 UV_FILL_COLOR = new Color32(49, 49, 49, 255);
+        readonly Color UV_FILL_COLOR = (Color) new Color32(49, 49, 49, 255);
 
         // This is the default background of the UV editor - used to compare bacground pixels when rendering UV template
         void InitiateScreenshot(int ImageSize, bool HideGrid, Color LineColor, bool TransparentBackground, Color BackgroundColor, bool RenderTexture)
