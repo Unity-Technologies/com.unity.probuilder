6efc3da3fd18e77992302a38a918086d1e312b2f
Claudia Antoun <claudia.antoun@unity3d.com>
2023-05-01 11:54:53 -0400

    Add a test to validate materials after deleting faces.

--- Tests/Runtime/MeshOps/DeleteElementsTests.cs
+++ Tests/Runtime/MeshOps/DeleteElementsTests.cs
@@ -87,6 +87,59 @@ static class DeleteElementsTests
     }
 
     [Test]
+    public static void DeleteFirstFaceAndValidateMaterials([ValueSource("shapeTypes")] Type shape)
+    {
+        var mesh = ShapeFactory.Instantiate(shape);
+
+        var redMat = new Material(Shader.Find("Standard"));
+        redMat.color = Color.red;
+        var greenMat = new Material(Shader.Find("Standard"));
+        greenMat.color = Color.green;
+
+        var materialCount = 0;
+        if (mesh.faceCount > 1)
+        {
+            mesh.renderer.sharedMaterials = new Material[] { redMat, greenMat };
+            materialCount = mesh.renderer.sharedMaterials.Length;
+            Assert.AreEqual(materialCount, 2);
+
+            mesh.facesInternal.FirstOrDefault().submeshIndex = 1; // green
+            for (int i = 1; i < mesh.facesInternal.Length; i++)
+                mesh.facesInternal[i].submeshIndex = 0; // red
+        }
+        else
+        {
+            mesh.renderer.sharedMaterials = new Material[] { redMat };
+            materialCount = mesh.renderer.sharedMaterials.Length;
+            Assert.AreEqual(materialCount, 1);
+
+            mesh.facesInternal.FirstOrDefault().submeshIndex = 1; // green
+        }
+
+        try
+        {
+            var face = mesh.facesInternal.FirstOrDefault();
+            int vertexCount = mesh.vertexCount;
+            int faceVertexCount = face.distinctIndexes.Count;
+            mesh.DeleteFace(face);
+            mesh.ToMesh();
+            mesh.Refresh();
+
+            TestUtility.AssertMeshAttributesValid(mesh.mesh);
+            Assert.AreEqual(mesh.vertexCount, vertexCount - faceVertexCount);
+            Assert.AreEqual(materialCount - 1, mesh.renderer.sharedMaterials.Length);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.ToString());
+        }
+        finally
+        {
+            UnityEngine.Object.DestroyImmediate(mesh.gameObject);
+        }
+    }
+
+    [Test]
     public static void DeleteAllFaces_CreatesValidMesh([ValueSource("shapeTypes")] Type shape)
     {
         var mesh = ShapeFactory.Instantiate(shape);
