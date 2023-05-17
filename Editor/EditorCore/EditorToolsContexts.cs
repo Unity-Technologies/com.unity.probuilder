551122848105cf9eb472392423750ea2545c7b2d
Karl Henkel <karlh@unity3d.com>
2023-05-17 15:36:59 -0400

    add context menu to probuilder tools

--- Editor/EditorCore/EditorToolsContexts.cs
+++ Editor/EditorCore/EditorToolsContexts.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor.EditorTools;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.UIElements;
 
 namespace UnityEditor.ProBuilder
 {
@@ -7,6 +11,66 @@ namespace UnityEditor.ProBuilder
     class PositionToolContext : EditorToolContext
     {
         PositionToolContext() { }
+
+        static DropdownMenuAction.Status GetStatus(MenuAction action)
+        {
+            if(action.hidden)
+                return DropdownMenuAction.Status.Hidden;
+            if (action.enabled)
+                return DropdownMenuAction.Status.Normal;
+            return DropdownMenuAction.Status.Disabled;
+        }
+
+        static readonly HashSet<string> k_ContextMenuBlacklist = new HashSet<string>()
+        {
+            "Smoothing",
+            "Material Editor",
+            "UV Editor",
+            "Vertex Colors"
+        };
+
+        public override void PopulateMenu(DropdownMenu menu)
+        {
+            menu.AppendHeaderAction(IconUtility.GetIcon("Modes/Mode_Vertex"),
+                x => { ProBuilderEditor.selectMode = SelectMode.Vertex; },
+                x => ProBuilderEditor.selectMode == SelectMode.Vertex
+                    ? DropdownMenuAction.Status.Checked
+                    : DropdownMenuAction.Status.Normal);
+            menu.AppendHeaderAction(IconUtility.GetIcon("Modes/Mode_Edge"),
+                x => { ProBuilderEditor.selectMode = SelectMode.Edge; },
+                x => ProBuilderEditor.selectMode == SelectMode.Edge
+                    ? DropdownMenuAction.Status.Checked
+                    : DropdownMenuAction.Status.Normal);
+            menu.AppendHeaderAction(IconUtility.GetIcon("Modes/Mode_Face"),
+                x => { ProBuilderEditor.selectMode = SelectMode.Face; },
+                x => ProBuilderEditor.selectMode == SelectMode.Face
+                    ? DropdownMenuAction.Status.Checked
+                    : DropdownMenuAction.Status.Normal);
+
+            var actions = EditorToolbarLoader.GetActions();
+            var group = ToolbarGroup.Tool;
+
+            // grouping and filtering is bespoke for demo reasons
+            foreach (var action in actions)
+            {
+                if (k_ContextMenuBlacklist.Contains(action.menuTitle))
+                    continue;
+
+                if (action.group == ToolbarGroup.Entity)
+                    continue;
+
+                if (action.group != group)
+                {
+                    menu.AppendSeparator();
+                    group = action.group;
+                }
+
+                if(action.group != ToolbarGroup.Geometry && action.group != ToolbarGroup.Tool)
+                    menu.AppendAction($"{action.group}/{action.menuTitle}", x => action.PerformAction(), GetStatus(action));
+                else
+                    menu.AppendAction(action.menuTitle, x => action.PerformAction(), GetStatus(action));
+            }
+        }
 
         protected override Type GetEditorToolType(Tool tool)
         {
