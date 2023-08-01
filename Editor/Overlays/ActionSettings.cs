84a93da9b15ddaccaade64a74df4a38d257f5566
Thomas Lopez <thomas.lopez@unity3d.com>
2023-08-01 13:41:19 -0400

    [STO-2994] Correctly handling Undo during action

--- Editor/Overlays/ActionSettings.cs
+++ Editor/Overlays/ActionSettings.cs
@@ -43,11 +43,7 @@ class MenuActionSettingsOverlay : Overlay
 
         if (m_Owner.HasPreview)
         {
-            var previewButton = new Button(() =>
-            {
-                Undo.PerformUndo();
-                m_CurrentAction.PerformAction();
-            });
+            var previewButton = new Button(() => m_Owner.UpdatePreview());
             previewButton.text = "Preview";
             previewButton.style.flexDirection = FlexDirection.Row;
             previewButton.style.flexGrow = 1;
@@ -68,10 +64,13 @@ public class MenuActionSettings : EditorAction
     bool m_Preview;
     internal bool HasPreview => m_Preview;
 
+    bool m_UndoNeeded = true;
+
     public MenuActionSettings(MenuAction action, bool hasPreview = true)
     {
         m_Action = action;
         m_Preview = hasPreview;
+        m_UndoNeeded = m_Preview;
 
         if(m_Preview)
             m_Action.PerformAction();
@@ -79,31 +78,53 @@ public class MenuActionSettings : EditorAction
         m_Overlay = new MenuActionSettingsOverlay(this, action);
         SceneView.AddOverlayToActiveView(m_Overlay);
         m_Overlay.displayed = true;
+
         // Pressing esc should escape the current action without applying, however, pressing ESC in PB is
         // returning from vert/edge/face mode to object mode, so when select mode is changed, we cancel the
         // action to mimic the behavior of EditorAction.
         ProBuilderEditor.selectModeChanged += (_) => Finish(EditorActionResult.Canceled);
 
-        // Ensure that if a MenuActionSettings is currently active, starting this one will not call
+        // Undo should undo the preview and leaving the action
+        Undo.undoRedoPerformed += UndoRedoPerformed;
+
+        // Delay call to ensure that if a MenuActionSettings is currently active, starting this one will not call
         // OnMenuActionPerformed when the other is deactivated
         EditorApplication.delayCall += () => MenuAction.onPerformAction += OnMenuActionPerformed;
+    }
+
+    protected override void OnFinish(EditorActionResult result)
+    {
+        MenuAction.onPerformAction -= OnMenuActionPerformed;
+        Undo.undoRedoPerformed -= UndoRedoPerformed;
+        SceneView.RemoveOverlayFromActiveView(m_Overlay);
+
+        if (!HasPreview && result == EditorActionResult.Success)
+            m_Action.PerformAction();
+        else if (m_UndoNeeded && result == EditorActionResult.Canceled)
+            EditorApplication.delayCall += Undo.PerformUndo;
+    }
+
+    internal void UpdatePreview()
+    {
+        if (HasPreview)
+        {
+            Undo.undoRedoPerformed -= UndoRedoPerformed;
+            Undo.PerformUndo();
+            m_Action.PerformAction();
+            Undo.undoRedoPerformed += UndoRedoPerformed;
+        }
+    }
+
+    void UndoRedoPerformed()
+    {
+        m_UndoNeeded = false;
+        Finish(EditorActionResult.Canceled);
     }
 
     void OnMenuActionPerformed(MenuAction action)
     {
         if (action != m_Action)
             Finish(EditorActionResult.Canceled);
-    }
-
-    protected override void OnFinish(EditorActionResult result)
-    {
-        MenuAction.onPerformAction -= OnMenuActionPerformed;
-        SceneView.RemoveOverlayFromActiveView(m_Overlay);
-
-        if (!HasPreview && result == EditorActionResult.Success)
-            m_Action.PerformAction();
-        else if (HasPreview && result == EditorActionResult.Canceled)
-            EditorApplication.delayCall += Undo.PerformUndo;
     }
 
     public override void OnSceneGUI(SceneView sceneView)
