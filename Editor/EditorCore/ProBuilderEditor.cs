af004c1d458ab2cf5fe9335257f3fed0872d381d
Claudia Antoun <claudia.antoun@unity3d.com>
2023-10-04 16:16:17 -0400

    Fix the mesh editing mouse click not working with the new shortcut improvements.

--- Editor/EditorCore/ProBuilderEditor.cs
+++ Editor/EditorCore/ProBuilderEditor.cs
@@ -567,7 +567,15 @@ namespace UnityEditor.ProBuilder
 
         internal void HandleMouseEvent(SceneView sceneView, int controlID)
         {
-            if(m_CurrentEvent.type == EventType.MouseDown && HandleUtility.nearestControl == controlID)
+#if UNITY_2023_1_OR_NEWER
+            bool isLeftMouseClick = m_CurrentEvent.type == EventType.MouseDown && m_CurrentEvent.keyCode == KeyCode.Mouse0 && HandleUtility.nearestControl == controlID;
+#elif UNITY_2022_3
+            KeyCode keyCode = m_CurrentEvent.isMouse ? m_CurrentEvent.button + KeyCode.Mouse0 : m_CurrentEvent.keyCode;
+            bool isLeftMouseClick = m_CurrentEvent.type == EventType.MouseDown && keyCode == KeyCode.Mouse0 && HandleUtility.nearestControl == controlID;
+#else
+            bool isLeftMouseClick = m_CurrentEvent.type == EventType.MouseDown && HandleUtility.nearestControl == controlID;
+#endif
+            if (isLeftMouseClick)
             {
                 // double clicking object
                 if(m_CurrentEvent.clickCount > 1)
