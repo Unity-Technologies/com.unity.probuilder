1365a8a31ea34154fdf74713d97549a34089a30f
Mod Rimkus <mod.rimkus@unity3d.com>
2025-02-06 13:34:35 -0500

    Updated test.

--- Tests/Editor/Actions/MergeObjectsTest.cs
+++ Tests/Editor/Actions/MergeObjectsTest.cs
@@ -12,15 +12,12 @@ public class MergeObjectsTest
 {
     ProBuilderMesh m_mesh1;
     ProBuilderMesh m_mesh2;
-    ProBuilderMesh m_mesh3;
     
     [SetUp]
     public void SetUp()
     {
         m_mesh1 = ShapeFactory.Instantiate<Cube>();
         m_mesh2 = ShapeFactory.Instantiate<Cone>();
-        m_mesh3 = ShapeFactory.Instantiate<Cylinder>();
-        m_mesh1.gameObject.AddComponent<BoxCollider>();
     }
 
     [TearDown]
@@ -28,39 +25,43 @@ public class MergeObjectsTest
     {
         UObject.DestroyImmediate(m_mesh1);
         UObject.DestroyImmediate(m_mesh2);
-        UObject.DestroyImmediate(m_mesh3);
     }
     
     [Test]
-    [TestCase(typeof(PolyShape))]
-    [TestCase(typeof(ProBuilderShape))]
-    public void MergeObjects_WithShapeCompPresent_ResultNoLongerHasShapeComp(System.Type shapeCompType)
+    [TestCase(typeof(PolyShape),typeof(PolyShape))] 
+    [TestCase(typeof(ProBuilderShape),typeof(ProBuilderShape))] 
+    [TestCase(typeof(ProBuilderShape),typeof(PolyShape))]
+    public void MergeObjects_WithShapeCompPresent_ResultNoLongerHasShapeComp(System.Type shapeCompTypeA, System.Type shapeCompTypeB)
     {
-        var meshes = new List<ProBuilderMesh>();
-        meshes.Add(m_mesh1);
-        meshes.Add(m_mesh2);
-        meshes.Add(m_mesh3);
-
-        foreach (var mesh in meshes)
+        void AttachComponentOfType(ProBuilderMesh mesh, System.Type shapeCompType)
         {
             if (shapeCompType == typeof(PolyShape)) 
                 mesh.gameObject.AddComponent<PolyShape>();
             else
                 mesh.gameObject.AddComponent<ProBuilderShape>();
         }
+        
+        var meshes = new List<ProBuilderMesh>();
+        meshes.Add(m_mesh1);
+        meshes.Add(m_mesh2);
+        
+        Assume.That(meshes.Count, Is.EqualTo(2));
 
-        MeshSelection.SetSelection(new List<GameObject>( new[]{ m_mesh1.gameObject, m_mesh2.gameObject, m_mesh3.gameObject }));
+        AttachComponentOfType(meshes[0], shapeCompTypeA);
+        AttachComponentOfType(meshes[1], shapeCompTypeB);
+        
+        MeshSelection.SetSelection(new List<GameObject>( new[]{ m_mesh1.gameObject, m_mesh2.gameObject }));
         ActiveEditorTracker.sharedTracker.ForceRebuild();
 
         var mergeObjectsAction = new MergeObjects();
         var newMeshes = mergeObjectsAction.DoMergeObjectsAction();
+        
+        Assume.That(newMeshes.Count, Is.EqualTo(1), "There should only be one mesh after merging objects.");
 
         var shapeCompFound = false; 
-        if (shapeCompType == typeof(PolyShape)) 
-            shapeCompFound = newMeshes[0].gameObject.GetComponent<PolyShape>() != null;
-        else 
-            shapeCompFound = newMeshes[0].gameObject.GetComponent<ProBuilderShape>() != null;
+        shapeCompFound |= newMeshes[0].gameObject.GetComponent<PolyShape>() != null;
+        shapeCompFound |= newMeshes[0].gameObject.GetComponent<ProBuilderShape>() != null;
         
-        Assert.That(shapeCompFound, Is.Not.True, $"There should be no {shapeCompType.Name} component on ProBuilder Meshes after mesh combine.");
+        Assert.That(shapeCompFound, Is.Not.True, $"There should be no {shapeCompTypeA.Name} or {shapeCompTypeB.Name} component on ProBuilder Meshes after mesh combine.");
     }
 }
