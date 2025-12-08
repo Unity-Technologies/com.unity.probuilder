d074c1ad36139c45a588e34326e1932c5917ee78
varinotmUnity <mathias.varinot@unity3d.com>
2025-12-08 11:10:26 -0500

    fix following PR comment

--- Editor/EditorCore/HierarchyListener.cs
+++ Editor/EditorCore/HierarchyListener.cs
@@ -26,15 +26,18 @@ namespace UnityEditor.ProBuilder
             {
                 var type = stream.GetEventType(i);
 
-                // Case 1: Creation or Structure Change
-                if (type == ObjectChangeKind.CreateGameObjectHierarchy ||
-                    type == ObjectChangeKind.ChangeGameObjectStructure)
+                if (type == ObjectChangeKind.CreateGameObjectHierarchy)
                 {
-                    GameObject go = GetGameObjectFromEvent(ref stream, i, type);
-                    if (go != null)
-                        CheckForProBuilderMeshesCreatedOrModified(go);
+                    stream.GetCreateGameObjectHierarchyEvent(i, out var data);
+                    var go = GetGameObject(data);
+                    if (go != null) CheckForProBuilderMeshesCreatedOrModified(go);
                 }
-                // Case 2: Hierarchy Structure Change
+                else if (type == ObjectChangeKind.ChangeGameObjectStructure)
+                {
+                    stream.GetChangeGameObjectStructureEvent(i, out var data);
+                    var go = GetGameObject(data);
+                    if (go != null) CheckForProBuilderMeshesCreatedOrModified(go);
+                }
                 else if (type == ObjectChangeKind.ChangeGameObjectStructureHierarchy)
                 {
                     // Note 2 : This needs to be called when using a Prefab>Replace action in the menus to refresh the current
@@ -42,8 +45,8 @@ namespace UnityEditor.ProBuilder
                     // Note 1 : that this is leaking meshes when reverting! in 2023.1+ we handle it correctly, but 2022 and
                     // 2021 have the PPtr reset to the serialized value (null) before we have any access. orphaned
                     // mesh assets are cleaned up on scene or domain reloads, so we'll live with it.
-                    
-                    GameObject go = GetGameObjectFromEvent(ref stream, i, type);
+                    stream.GetChangeGameObjectStructureHierarchyEvent(i, out var data);
+                    var go = GetGameObject(data);
 
                     if (go != null)
                     {
@@ -51,66 +54,27 @@ namespace UnityEditor.ProBuilder
                         foreach (var mesh in meshes)
                             EditorUtility.SynchronizeWithMeshFilter(mesh);
                     }
-
                     ProBuilderEditor.Refresh();
                 }
             }
         }
-		
-		private static GameObject GetGameObjectFromEvent(ref ObjectChangeEventStream stream, int index, ObjectChangeKind kind)
-		{
-		#if UNITY_6000_4_OR_NEWER
-			switch (kind)
-			{
-				case ObjectChangeKind.CreateGameObjectHierarchy:
-				{
-					stream.GetCreateGameObjectHierarchyEvent(index, out var data);
-					return UnityEditor.EditorUtility.EntityIdToObject(data.entityId) as GameObject;
-				}
-				case ObjectChangeKind.ChangeGameObjectStructure:
-				{
-					stream.GetChangeGameObjectStructureEvent(index, out var data);
-					return UnityEditor.EditorUtility.EntityIdToObject(data.entityId) as GameObject;
-				}
-				case ObjectChangeKind.ChangeGameObjectStructureHierarchy:
-				{
-					stream.GetChangeGameObjectStructureHierarchyEvent(index, out var data);
-					return UnityEditor.EditorUtility.EntityIdToObject(data.entityId) as GameObject;
-				}
-			}
-		#else
-			int instanceId = 0;
-			switch (kind)
-			{
-				case ObjectChangeKind.CreateGameObjectHierarchy:
-				{
-					stream.GetCreateGameObjectHierarchyEvent(index, out var data);
-					instanceId = data.instanceId;
-					break;
-				}
-				case ObjectChangeKind.ChangeGameObjectStructure:
-				{
-					stream.GetChangeGameObjectStructureEvent(index, out var data);
-					instanceId = data.instanceId;
-					break;
-				}
-				case ObjectChangeKind.ChangeGameObjectStructureHierarchy:
-				{
-					stream.GetChangeGameObjectStructureHierarchyEvent(index, out var data);
-					instanceId = data.instanceId;
-					break;
-				}
-			}
 
-			if (instanceId != 0)
-			{
-		#pragma warning disable CS0618 // Type or member is obsolete
-				return UnityEditor.EditorUtility.InstanceIDToObject(instanceId) as GameObject;
-		#pragma warning restore CS0618
-			}
-		#endif
-			return null;
-		}
+#if UNITY_6000_4_OR_NEWER
+        static GameObject GetGameObject(CreateGameObjectHierarchyEventArgs data) => UnityEditor.EditorUtility.EntityIdToObject(data.entityId) as GameObject;
+        static GameObject GetGameObject(ChangeGameObjectStructureEventArgs data) => UnityEditor.EditorUtility.EntityIdToObject(data.entityId) as GameObject;
+        static GameObject GetGameObject(ChangeGameObjectStructureHierarchyEventArgs data) => UnityEditor.EditorUtility.EntityIdToObject(data.entityId) as GameObject;
+#else
+        static GameObject GetGameObject(CreateGameObjectHierarchyEventArgs data) => InstanceIDToObject(data.instanceId);
+        static GameObject GetGameObject(ChangeGameObjectStructureEventArgs data) => InstanceIDToObject(data.instanceId);
+        static GameObject GetGameObject(ChangeGameObjectStructureHierarchyEventArgs data) => InstanceIDToObject(data.instanceId);
+
+        static GameObject InstanceIDToObject(int instanceId)
+        {
+#pragma warning disable CS0618
+            return UnityEditor.EditorUtility.InstanceIDToObject(instanceId) as GameObject;
+#pragma warning restore CS0618
+        }
+#endif
 
         static void CheckForProBuilderMeshesCreatedOrModified(GameObject go)
         {
