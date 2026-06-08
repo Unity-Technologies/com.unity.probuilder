d266adf99aeb3ea23fb6035bc76532da6278d9d5
Thomas Lopez <thomas.lopez@unity3d.com>
2026-06-08 15:11:51 -0400

    tmp removing macos13arm from pull request trigger job list

--- .Tools/CI/Recipes/Triggers.cs
+++ .Tools/CI/Recipes/Triggers.cs
@@ -22,7 +22,7 @@ public class Triggers : RecipeBase
         HashSet<IJobBuilder> builders =
         [
             JobBuilder.Create("Pull Request Trigger")
-                .WithDependencies(allValidationJobs.Where(d => d.JobId.Contains("macos") || d.JobId.Contains("windows")))
+                .WithDependencies(allValidationJobs.Where(d => (d.JobId.Contains("macos") && !d.JobId.Contains("6000.6")) || d.JobId.Contains("win10")))
                 .WithPullRequestTrigger(pr => pr.ExcludeDraft().And().WithTargetBranch("master"),
                     true, cancelLeftoverJobs: CancelLeftoverJobs.Always)
         ];
