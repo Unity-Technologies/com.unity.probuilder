3307a015ce2c8b9b668fbb2e63249ebc3768192a
Thomas Lopez <thomas.lopez@unity3d.com>
2026-06-09 10:06:54 -0400

    removing failing vm

--- .Tools/CI/Recipes/Triggers.cs
+++ .Tools/CI/Recipes/Triggers.cs
@@ -22,7 +22,7 @@ public class Triggers : RecipeBase
         HashSet<IJobBuilder> builders =
         [
             JobBuilder.Create("Pull Request Trigger")
-                .WithDependencies(allValidationJobs.Where(d => d.JobId.Contains("macos") || d.JobId.Contains("win10")))
+                .WithDependencies(allValidationJobs.Where(d => (d.JobId.Contains("macos") && !d.JobId.Contains("6000.6")) ))
                 .WithPullRequestTrigger(pr => pr.ExcludeDraft().And().WithTargetBranch("master"),
                     true, cancelLeftoverJobs: CancelLeftoverJobs.Always)
         ];
